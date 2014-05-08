fa57b96b7acc8a1f9a2a94c65977b9ce7c6d45b1
Harri Rautila <harri.rautila@gmail.com>
2014-05-09 01:59:53 +0300

    New constants.

--- src/armas/armas.h
+++ src/armas/armas.h
@@ -10,6 +10,7 @@
 #ifndef _ARMAS_H_INCLUDED
 #define _ARMAS_H_INCLUDED
 
+#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -20,6 +21,7 @@ enum armas_flags {
   ARMAS_NOTRANS = 0,
   ARMAS_NULL    = 0,
   ARMAS_NONE    = 0,
+  ARMAS_ANY     = 0,
   ARMAS_TRANSA  = 0x1,     ///< operand A is transposed
   ARMAS_TRANSB  = 0x2,     ///< operand B is transposed
   ARMAS_TRANS   = 0x4,     ///< matrix operand is transposed
@@ -32,7 +34,9 @@ enum armas_flags {
   ARMAS_CONJB   = 0x200,   ///< operand B is conjugate transposed
   ARMAS_CONJ    = 0x400,   ///< matrix operand is conjugate transposed
   ARMAS_SYMM    = 0x800,   ///< symmetric matrix
-  ARMAS_HERM    = 0x1000   ///< hermitian matrix
+  ARMAS_HERM    = 0x1000,  ///< hermitian matrix
+  ARMAS_MULTQ   = 0x2000,  ///< multiply with Q in bidiagonal 
+  ARMAS_MULTP   = 0x4000,  ///< multiply with P in bidiagonal 
 };
 
 enum armas_opts {
@@ -50,13 +54,17 @@ enum armas_errors {
   ARMAS_ESIZE        = 1,  ///< operand size mismatch
   ARMAS_ENEED_VECTOR = 2,  ///< vector operand required
   ARMAS_EINVAL = 3,        ///< invalid parameter
-  ARMAS_EIMP = 4           ///< not implemented
+  ARMAS_EIMP = 4,          ///< not implemented
+  ARMAS_EWORK = 5,         ///< workspace to small
+  ARMAS_ESINGULAR = 6,     ///< singular matrix
+  ARMAS_ENEGATIVE = 7      ///< negative value on diagonal
 };
 
 enum armas_norms {
   ARMAS_NORM_ONE = 1,
   ARMAS_NORM_TWO = 2,
-  ARMAS_NORM_INF = 3
+  ARMAS_NORM_INF = 3,
+  ARMAS_NORM_FRB = 4
 };
 
 #ifndef MAX_CPU
@@ -67,10 +75,10 @@ enum armas_norms {
  * @brief Configuration parameters
  */
 typedef struct armas_conf {
-  int mb;        ///< block size relative to result matrix rows
-  int nb;        ///< block size relative to result matrix cols
-  int kb;        ///< block size relative to operand matrix common dimension 
-  int lb;        ///< block size for blocked algorithms 
+  int mb;        ///< block size relative to result matrix rows (blas3)
+  int nb;        ///< block size relative to result matrix cols (blas3)
+  int kb;        ///< block size relative to operand matrix common dimension (blas3)
+  int lb;        ///< block size for blocked algorithms (lapack)
   int maxproc;   ///< max processors to use
   int error;     ///< last error
   int optflags;  ///< config options
@@ -92,30 +100,28 @@ extern int armas_last_error();
 /**
  * @brief Pivot vector
  */
-typedef struct armas_pivots {
+typedef struct armas_pivot {
   int npivots;    ///< Pivot storage size
   int *indexes;   ///< Pivot storage
   int owner;      ///< Storage owner flag
-} armas_pivots_t;
+} armas_pivot_t;
 
 #ifndef __INLINE
 #define __INLINE extern inline
 #endif
 
 /**
- * @brief Create new pivot vector, allocates new storage.
+ * @brief Initialize pivot vector, allocates new storage.
  */
 __INLINE
-armas_pivots_t *armas_pivot_new(int sz)
+armas_pivot_t *armas_pivot_init(armas_pivot_t *ptable, int sz)
 {
-  armas_pivots_t *ptable = (armas_pivots_t*)malloc(sizeof(armas_pivots_t));
   if (!ptable)
     return ptable;
 
   ptable->indexes = (int *)calloc(sz, sizeof(int));
   if (!ptable->indexes) {
-    free(ptable);
-    return (armas_pivots_t *)0;
+    return (armas_pivot_t *)0;
   }
   ptable->npivots = sz;
   ptable->owner = 1;
@@ -123,10 +129,27 @@ armas_pivots_t *armas_pivot_new(int sz)
 }
 
 /**
+ * @brief Create new pivot vector, allocates new storage.
+ */
+__INLINE
+armas_pivot_t *armas_pivot_new(int sz)
+{
+  armas_pivot_t *ptable = (armas_pivot_t*)malloc(sizeof(armas_pivot_t));
+  if (!ptable)
+    return ptable;
+
+  if (! armas_pivot_init(ptable, sz) ) {
+    free(ptable);
+    return (armas_pivot_t *)0;
+  }
+  return ptable;
+}
+
+/**
  * @brief Setup pivot vector with given storage.
  */
 __INLINE
-armas_pivots_t *armas_pivot_make(armas_pivots_t *ptable, int sz, int *data)
+armas_pivot_t *armas_pivot_make(armas_pivot_t *ptable, int sz, int *data)
 {
   ptable->npivots = sz;
   ptable->indexes = data;
@@ -138,11 +161,12 @@ armas_pivots_t *armas_pivot_make(armas_pivots_t *ptable, int sz, int *data)
  * @brief Release pivot vector storage.
  */
 __INLINE
-void armas_pivot_release(armas_pivots_t *ptable)
+void armas_pivot_release(armas_pivot_t *ptable)
 {
   if (ptable && ptable->owner) {
     free(ptable->indexes);
     ptable->indexes = (int *)0;
+    ptable->npivots = 0;
   }
 }
 
@@ -150,7 +174,7 @@ void armas_pivot_release(armas_pivots_t *ptable)
  * @brief Free pivot vector.
  */
 __INLINE
-void armas_pivot_free(armas_pivots_t *ptable)
+void armas_pivot_free(armas_pivot_t *ptable)
 {
   if (!ptable)
     return;
@@ -163,7 +187,7 @@ void armas_pivot_free(armas_pivots_t *ptable)
  * @brief Pivot vector size.
  */
 __INLINE
-int armas_pivot_size(armas_pivots_t *ptable)
+int armas_pivot_size(armas_pivot_t *ptable)
 {
   return ptable ? ptable->npivots : 0;
 }
@@ -172,12 +196,31 @@ int armas_pivot_size(armas_pivots_t *ptable)
  * @brief Get raw pivot storage.
  */
 __INLINE
-int *armas_pivot_data(armas_pivots_t *ptable)
+int *armas_pivot_data(armas_pivot_t *ptable)
 {
   return ptable ? ptable->indexes : (int *)0;
 }
 
+/**
+ * @brief Get pivot at index k.
+ */
+__INLINE
+int armas_pivot_get(armas_pivot_t *ptable, int k)
+{
+  return ptable && k >= 0 && k < ptable->npivots ? ptable->indexes[k] : 0;
+}
 
+/**
+ * @brief Set pivot at index k.
+ */
+__INLINE
+void armas_pivot_set(armas_pivot_t *ptable, int k, int val)
+{
+  if (ptable && k >= 0 && k < ptable->npivots)
+    ptable->indexes[k] = val;
+}
+
+extern void armas_pivot_printf(FILE* out, const char *fmt, armas_pivot_t *pivot);
 
 
 #endif
