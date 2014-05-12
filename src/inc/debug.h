2ff1f44770f9060855dbbe9f08e3a958e691320a
Harri Rautila <harri.rautila@gmail.com>
2014-05-13 00:54:42 +0300

    Debugging macros.

---
+++ src/inc/debug.h
@@ -0,0 +1,57 @@
+
+// Copyright (c) Harri Rautila, 2012,2013,2014
+
+// This file is part of github.com/hrautila/armas library. It is free software,
+// distributed under the terms of GNU Lesser General Public License Version 3, or
+// any later version. See the COPYING tile included in this archive.
+
+#ifndef __ARMAS_DEBUG_H
+#define __ARMAS_DEBUG_H 1
+
+#include <math.h>
+
+#if defined(COMPLEX128)
+static inline void __check_nan(double complex val) {}
+static inline void __check_mm256(__m256d val) {}
+
+#elif defined(COMPLEX64)
+static inline void __check_nan(float complex val) {}
+static inline void __check_mm256(__m256d val) {}
+
+#elif defined(FLOAT32)
+static inline void __check_nan(float val) {}
+static inline void __check_mm256(__m256d val) {}
+
+#else // FLOAT(64)
+static inline void __check_nan(double val) {
+  double *ptr = 0;
+  // if NaN then SIGSEGV 
+  if (isnan(val))
+    *ptr = val;
+}
+
+static inline void __check_mm256(__m256d val) {
+  __check_nan(val[0]);
+  __check_nan(val[1]);
+  __check_nan(val[2]);
+  __check_nan(val[3]);
+}
+
+#endif
+
+#if defined(__DEBUG__)
+#define CHECK_NAN(val)  __check_nan(val)
+
+#define CHECK_MM256(val) __check_mm256(val)
+#else
+#define CHECK_NAN(val)
+#define CHECK_MM256(val)
+
+#endif // defined(__DEBUG__)
+
+#endif // __ARMAS_DEBUG_H
+  
+
+// Local Variables:
+// indent-tabs-mode: nil
+// End:
