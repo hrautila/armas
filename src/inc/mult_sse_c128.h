70d532c7ea820aa30701b36a0ea9f3194aa15d5c
Harri Rautila <harri.rautila@gmail.com>
2014-05-13 23:54:30 +0300

    Header files for instruction set optimized versions.

---
+++ src/inc/mult_sse_c128.h
@@ -0,0 +1,17 @@
+
+// Copyright (c) Harri Rautila, 2012-2014
+
+// This file is part of github.com/armas package. It is free software,
+// distributed under the terms of GNU Lesser General Public License Version 3, or
+// any later version. See the COPYING file included in this archive.
+
+#ifndef __MULT_SSE_C128_H
+#define __MULT_SSE_C128_H 1
+
+// SSE instruction set optimized versions of microblock computation routines
+// for double precision complex numbers
+
+// not yet implemented, include NON-SIMD versions instead
+#include "mult_nosimd.h"
+
+#endif /* __MULT_SSE_C128_H */
