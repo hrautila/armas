4dccb234b24d0c191157267afbefc7b103c10016
Harri Rautila <harri.rautila@gmail.com>
2014-09-14 01:53:13 +0300

    New and updated tests.

--- tests/unit/trdevd.c
+++ tests/unit/trdevd.c
@@ -9,47 +9,54 @@
 #include <armas/dmatrix.h>
 #include "helper.h"
 
-#define NAME "bdsvd"
+#define NAME "trdevd"
 
-int set_diagonals(armas_d_dense_t *A, int flags, int type)
+int set_diagonals(armas_d_dense_t *A, int flags, int type, double coeff)
 {
-    armas_d_dense_t sD, sE;
+    armas_d_dense_t sD, sE0, sE1;
     int k, N, S, E;
 
     armas_d_diag(&sD, A, 0);
-    armas_d_diag(&sE, A, (flags & ARMAS_LOWER ? -1 : 1));
+    armas_d_diag(&sE0, A, 1);
+    armas_d_diag(&sE1, A, -1);
     N = armas_d_size(&sD);
 
     switch (type) {
     case 2:
-        // bidiagonal larger values in the middle
+        // larger values in the middle
         S = E = 0;
         for (k = 0; k < N-1; k++) {
             if (k & 0x1) {
-                armas_d_set_at(&sD, N-1-E, k+1);
+                armas_d_set_at(&sD, N-1-E, coeff*(k+1));
                 E++;
             } else {
-                armas_d_set_at(&sD, S, k+1);
+                armas_d_set_at(&sD, S, coeff*(k+1));
                 S++;
             }
-            if (k < N-1)
-                armas_d_set_at(&sE, k, 1.0);
+            if (k < N-1) {
+                armas_d_set_at(&sE0, k, 1.0);
+                armas_d_set_at(&sE1, k, 1.0);
+            }
         }
         break;
     case 1:
-        // bidiagonal larger values on top
+        // larger values on top
         for (k = 0; k < N-1; k++) {
-            armas_d_set_at(&sD, N-1-k, k+1);
-            if (k < N-1)
-                armas_d_set_at(&sE, k, 1.0);
+            armas_d_set_at(&sD, N-1-k, (k+1)*coeff);
+            if (k < N-1) {
+                armas_d_set_at(&sE0, k, 1.0);
+                armas_d_set_at(&sE1, k, 1.0);
+            }
         }
         break;
     default:
-        // bidiagonal larger values on bottom
+        // larger values on bottom
         for (k = 0; k < N; k++) {
-            armas_d_set_at(&sD, k, k+1);
-            if (k < N-1)
-                armas_d_set_at(&sE, k, 1.0);
+            armas_d_set_at(&sD, k, (k+1)*coeff);
+            if (k < N-1) {
+                armas_d_set_at(&sE0, k, 1.0);
+                armas_d_set_at(&sE1, k, 1.0);
+            }
         }
     }
 }
@@ -65,31 +72,28 @@ void abs_minus(armas_d_dense_t *D0, armas_d_dense_t *D1)
     }
 }
 
-// test: M >= N
-int test_tall(int M, int N, int flags, int type, int verbose)
+// test: 
+int test_eigen(int N, int type, double coeff, int verbose)
 {
-    armas_d_dense_t A0, At, U, V, D, E, sD, sE, C, W;
+    armas_d_dense_t A0, At, U, V, D, E, sD, sE0, sE1, C, W, t;
     armas_conf_t conf = *armas_conf_default();
     double nrm;
-    int ok, fails = 0;
-    char *desc = flags & ARMAS_LOWER ? "lower" : "upper";
-
-    armas_d_init(&A0, M, N);
-    set_diagonals(&A0, flags, type);
-    armas_d_submatrix(&At, &A0, 0, 0, N, N);
+    int ok, fails = 0, err;
+    char desc[6] = "typeX";
+    desc[4] = '0' + type;
+        
+    armas_d_init(&A0, N, N);
+    set_diagonals(&A0, 0, type, coeff);
 
     armas_d_init(&D, N, 1);
     armas_d_init(&E, N-1, 1);
     armas_d_diag(&sD, &A0, 0);
-    armas_d_diag(&sE, &A0, (flags & ARMAS_LOWER ? -1 : 1));
+    armas_d_diag(&sE0, &A0, 1);
+    armas_d_diag(&sE1, &A0, -1);
     armas_d_copy(&D, &sD, &conf);
-    armas_d_copy(&E, &sE, &conf);
+    armas_d_copy(&E, &sE0, &conf);
 
     // unit singular vectors
-    armas_d_init(&U, M, N);
-    armas_d_diag(&sD, &U, 0);
-    armas_d_madd(&sD, 1.0, 0);
-
     armas_d_init(&V, N, N);
     armas_d_diag(&sD, &V, 0);
     armas_d_madd(&sD, 1.0, 0);
@@ -97,52 +101,38 @@ int test_tall(int M, int N, int flags, int type, int verbose)
     armas_d_init(&C, N, N);
     armas_d_init(&W, 4*N, 1);
 
-    armas_d_bdsvd(&D, &E, &U, &V, &W, flags|ARMAS_WANTU|ARMAS_WANTV, &conf);
-
+    armas_d_trdeigen(&D, &E, &V, &W, ARMAS_WANTV, &conf);
     // compute: U.T*A*V
-    armas_d_mult(&C, &U, &A0, 1.0, 0.0, ARMAS_TRANSA, &conf);
-    armas_d_mult(&At, &C, &V, 1.0, 0.0, ARMAS_TRANSB, &conf);
+    armas_d_mult(&C, &V, &A0, 1.0, 0.0, ARMAS_TRANSA, &conf);
+    armas_d_mult(&A0, &C, &V, 1.0, 0.0, ARMAS_NOTRANS, &conf);
 
     if (verbose > 2 && N < 10) {
         printf("D:\n"); armas_d_printf(stdout, "%6.3f", &D);
-        printf("At:\n"); armas_d_printf(stdout, "%6.3f", &At);
+        printf("V.T*A*V:\n"); armas_d_printf(stdout, "%6.3f", &A0);
     }
 
-    // compute ||U.T*A*V - S|| (D is column vector, sD is row vector)
-    armas_d_diag(&sD, &At, 0);
-    abs_minus(&sD, &D);
+    // compute ||V.T*A*V - S|| (D is column vector, sD is row vector)
+    armas_d_diag(&sD, &A0, 0);
+    armas_d_axpy(&sD, &D, -1.0, &conf);
     nrm = armas_d_mnorm(&sD, ARMAS_NORM_ONE, &conf);
     ok = isFINE(nrm, N*1e-10);
-    printf("%s: [%s] U.T*A*V == S\n", PASS(ok), desc);
+    printf("%s: [%s] V.T*A*V == eigen(A)\n", PASS(ok), desc);
     if (verbose > 0)
-        printf("  M=%d, N=%d ||U.T*A*V - S||_1: %e\n", M, N, nrm);
+        printf("  N=%d ||V.T*A*V - eigen(A)||_1: %e\n", N, nrm);
 
     if (!ok)
         fails++;
-
-    // compute: ||I - U.T*U||
-    armas_d_mult(&C, &U, &U, 1.0, 0.0, ARMAS_TRANSA, &conf);
-    armas_d_diag(&sD, &C, 0);
-    armas_d_madd(&sD, -1.0, 0);
-
-    nrm = armas_d_mnorm(&C, ARMAS_NORM_ONE, &conf);
-    ok = isFINE(nrm, N*1e-15);
-    printf("%s: I == U.T*U\n", PASS(ok));
-    if (verbose > 0)
-        printf("  M=%d, N=%d ||I - U.T*U||_1: %e\n", M, N, nrm);
-    if (!ok)
-        fails++;
-
 
     // compute ||I - V*V.T||_1
     armas_d_mult(&C, &V, &V, 1.0, 0.0, ARMAS_TRANSA, &conf);
+    armas_d_diag(&sD, &C, 0);
     armas_d_madd(&sD, -1.0, 0);
 
     nrm = armas_d_mnorm(&C, ARMAS_NORM_ONE, &conf);
     ok = isFINE(nrm, N*1e-15);
     printf("%s: I == V*V.T\n", PASS(ok));
     if (verbose > 0)
-        printf("  M=%d, N=%d ||I - V*V.T||_1: %e\n", M, N, nrm);
+        printf("  N=%d ||I - V*V.T||_1: %e\n", N, nrm);
     if (!ok)
         fails++;
 
@@ -159,11 +149,15 @@ main(int argc, char **argv)
     int ok = 0;
     int nproc = 1;
     int verbose = 0;
+    double coeff = 1.0;
 
-    while ((opt = getopt(argc, argv, "P:v")) != -1) {
+    while ((opt = getopt(argc, argv, "P:c:v")) != -1) {
         switch (opt) {
         case 'P':
             nproc = atoi(optarg);
+            break;
+        case 'c':
+            coeff = atof(optarg);
             break;
         case 'v':
             verbose += 1;
@@ -188,9 +182,7 @@ main(int argc, char **argv)
     }
 
     int fails = 0;
-    if (test_tall(M, N, ARMAS_UPPER, 0, verbose))
-        fails++;
-    if (test_tall(M, N, ARMAS_LOWER, 0, verbose))
+    if (test_eigen(N, 0, coeff, verbose))
         fails++;
 
     exit(fails);
