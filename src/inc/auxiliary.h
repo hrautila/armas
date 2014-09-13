bf24a45b6d61519891b0cd7887eb24ea9d903666
Harri Rautila <harri.rautila@gmail.com>
2014-09-14 01:45:08 +0300

    New QR/QL iterators.

--- src/inc/auxiliary.h
+++ src/inc/auxiliary.h
@@ -82,6 +82,7 @@ void __gvrot(DTYPE *v0, DTYPE *v1, DTYPE cos, DTYPE sin, DTYPE y0, DTYPE y1)
  *
  */
 
+#if !defined(__bd_qrsweep_enhanced)
 /*
  * \brief Bidiagonal top to bottom QR sweep.
  *
@@ -102,7 +103,6 @@ void __gvrot(DTYPE *v0, DTYPE *v1, DTYPE cos, DTYPE sin, DTYPE y0, DTYPE y1)
  *
  * \ingroup lapackaux internal
  */
-#if !defined(__bd_qrsweep_enhanced)
 static inline
 int __bd_qrsweep(__armas_dense_t *D, __armas_dense_t *E,
                  __armas_dense_t *Cr, __armas_dense_t *Sr,
@@ -119,7 +119,7 @@ int __bd_qrsweep(__armas_dense_t *D, __armas_dense_t *E,
     for (k = 0; k < N-1; k++) {
         d2 = __armas_get_at_unsafe(D, k+1);
 
-        __armas_gvcompute(&cosr, &sinr, &r, f, g);
+        __gvrotg(&cosr, &sinr, &r, f, g);
         if (k > 0) {
             // e[i-1] = r
             __armas_set_at_unsafe(E, k-1, r);
@@ -128,7 +128,7 @@ int __bd_qrsweep(__armas_dense_t *D, __armas_dense_t *E,
         __gvrot(&f, &e1, cosr, sinr, d1, e1);
         // g = sinr*d[i+1]; d[i+1] = cosr*d[i+];
         __gvrot(&g, &d2, cosr, sinr, 0.0, d2);
-        __armas_gvcompute(&cosl, &sinl, &r, f, g);
+        __gvrotg(&cosl, &sinl, &r, f, g);
         // d[i] = r
         d1 = r;
         // f = cosl*e[i] + sinl*d[i+1]; d[i+1] = cosl*d[i+1] - sinl*e[i];
@@ -154,11 +154,17 @@ int __bd_qrsweep(__armas_dense_t *D, __armas_dense_t *E,
     }
     // e[-1] = f
     __armas_set_at_unsafe(E, N-2, f);
-    return k > 0 ? k+1 : 0;
+    //return k > 0 ? k+1 : 0;
+    return N-1;
 }
 #endif // !defined(__bd_qrsweep_enhanced)
 
 #if !defined(__bd_qrzero_enhanced)
+/*
+ * \brief Implicit zero shift QR sweep.
+ *
+ * As described in Demmel-Kahan, 1990.
+ */
 static inline
 int __bd_qrzero(__armas_dense_t *D, __armas_dense_t *E,
                  __armas_dense_t *Cr, __armas_dense_t *Sr,
@@ -174,10 +180,10 @@ int __bd_qrzero(__armas_dense_t *D, __armas_dense_t *E,
     for (k = 0; k < N-1; k++) {
         e1 = __armas_get_at_unsafe(E, k);
         d2 = __armas_get_at_unsafe(D, k+1);
-        __armas_gvcompute(&cosr, &sinr, &r, d1*cosr, e1);
+        __gvrotg(&cosr, &sinr, &r, d1*cosr, e1);
         if (k > 0)
             __armas_set_at_unsafe(E, k-1, sinl*r);
-        __armas_gvcompute(&cosl, &sinl, &r, cosl*r, sinr*d2);
+        __gvrotg(&cosl, &sinl, &r, cosl*r, sinr*d2);
         __armas_set_at_unsafe(D, k, r);
         d1 = d2;
         if (saves) {
@@ -190,8 +196,201 @@ int __bd_qrzero(__armas_dense_t *D, __armas_dense_t *E,
     d2 = cosr*d2;
     __armas_set_at_unsafe(D, N-1, d2*cosl);
     __armas_set_at_unsafe(E, N-2, d2*sinl);
+    //return k > 0 ? k+1 : 0;
+    return N-1;
 }
 #endif
+
+#if ! defined(__bd_qlsweep_enhanced)
+static inline
+int __bd_qlsweep(__armas_dense_t *D, __armas_dense_t *E,
+                 __armas_dense_t *Cr, __armas_dense_t *Sr,
+                 __armas_dense_t *Cl, __armas_dense_t *Sl, DTYPE f0, DTYPE g0, int saves)
+{
+    DTYPE d1, e1, d2, e2, f, g,  cosr, cosl, sinr, sinl, r;
+    int k, n, N = __armas_size(D);
+
+    d1 = __armas_get_at_unsafe(D, N-1);
+    e1 = __armas_get_at_unsafe(E, N-2);
+    f = f0;
+    g = g0;
+    for (n = 0, k = N-1; k > 0; k--, n++) {
+        d2 = __armas_get_at_unsafe(D, k-1);
+
+        __gvrotg(&cosr, &sinr, &r, f, g);
+        if (k < N-1) {
+            __armas_set_at_unsafe(E, k, r);
+        }
+        //f  = cosr*d1 + sinr*e1;   e1 = cosr*e1 - sinr*d1;
+        __gvrot(&f, &e1, cosr, sinr, d1, e1);
+        //g  = sinr*d2;  d2 = cosr*d2;
+        __gvrot(&g, &d2, cosr, sinr, 0.0, d2);
+
+        __gvrotg(&cosl, &sinl, &r, f, g);
+        d1 = r;
+        //f  = cosl*e1 + sinl*d2;  d2 = cosl*d2 - sinl*e1;
+        __gvrot(&f, &d2, cosl, sinl, e1, d2);
+        if (k > 1) {
+            e2 = __armas_get_at_unsafe(E, k-2);
+            //g  = sinl*e2;  e2 = cosl*e2;
+            __gvrot(&g, &e2, cosl, sinl, 0.0, e2);
+            __armas_set_at_unsafe(E, k-2, e2);
+        }
+
+        __armas_set_at_unsafe(D, k, d1);
+        __armas_set_at_unsafe(D, k-1, d2);
+        // save values;
+        d1 = d2; e1 = e2;
+        // save rotations
+        if (saves) {
+            __armas_set_at_unsafe(Cr, k-1, cosr);
+            __armas_set_at_unsafe(Sr, k-1, -sinr);
+            __armas_set_at_unsafe(Cl, k-1, cosl);
+            __armas_set_at_unsafe(Sl, k-1, -sinl);
+        }
+    }
+    __armas_set_at_unsafe(E, 0, f);
+    //return n > 0 ? N : 0;
+    return N-1;
+}
+#endif
+
+
+#if ! defined(__bd_qlzero_enhanced)
+static inline
+int __bd_qlzero(__armas_dense_t *D, __armas_dense_t *E,
+                __armas_dense_t *Cr, __armas_dense_t *Sr,
+                __armas_dense_t *Cl, __armas_dense_t *Sl, 
+                int saves)
+{
+    DTYPE d1, e1, d2, cosr, cosl, sinr, sinl, r;
+    int k, n, N = __armas_size(D);
+
+    d1 = __armas_get_at_unsafe(D, N-1);
+    cosr = __ONE;
+    cosl = __ONE;
+    for (n = 0, k = N-1; k > 0; k--, n++) {
+        e1 = __armas_get_at_unsafe(E, k-1);
+        d2 = __armas_get_at_unsafe(D, k-1);
+        __gvrotg(&cosr, &sinr, &r, d1*cosr, e1);
+        if (k < N-1)
+            __armas_set_at_unsafe(E, k, sinl*r);
+        __gvrotg(&cosl, &sinl, &r, cosl*r, sinr*d2);
+        __armas_set_at_unsafe(D, k, r);
+        d1 = d2;
+        if (saves) {
+            __armas_set_at_unsafe(Cr, k-1, cosr);
+            __armas_set_at_unsafe(Sr, k-1, -sinr);
+            __armas_set_at_unsafe(Cl, k-1, cosl);
+            __armas_set_at_unsafe(Sl, k-1, -sinl);
+        }
+    }
+    d2 = cosr*__armas_get_at_unsafe(D, 0);
+    __armas_set_at_unsafe(D, 0, d2*cosl);
+    __armas_set_at_unsafe(E, 0, d2*sinl);
+    //return n > 0 ? N : 0;
+    return N-1;
+}
+
+
+#endif
+
+#if !defined(__trd_qlsweep_enhanced)
+/*
+ * \brief Implicit tridiagonal QL sweep from bottom to top.
+ */
+static inline
+int __trd_qlsweep(__armas_dense_t *D, __armas_dense_t *E, __armas_dense_t *Cr,
+                  __armas_dense_t *Sr, DTYPE f0, DTYPE g0, int saves)
+{
+    DTYPE cosr, sinr, r, d0, d1, e0, e1, e0r, e0c, w0, f, g; 
+    int k;
+    int N = __armas_size(D);
+
+    f = f0; g = g0;
+    d0 = __armas_get_at_unsafe(D, N-1);
+    e0 = __armas_get_at_unsafe(E, N-2);
+
+    for (k = N-1; k > 0; k--) {
+        d1 = __armas_get_at_unsafe(D, k-1);
+        __gvrotg(&cosr, &sinr, &r, f, g);
+        if (k < N-1)
+            __armas_set_at_unsafe(E, k, r);
+        // rows
+        __gvrot(&d0,  &e0c, cosr, sinr, d0, e0);
+        __gvrot(&e0r, &d1,  cosr, sinr, e0, d1);
+        __gvrot(&d0,  &e0r, cosr, sinr, d0, e0r);
+        __gvrot(&e0c, &d1,  cosr, sinr, e0c, d1);
+        if (k > 1) {
+            e1 = __armas_get_at_unsafe(E, k-2);
+            __gvrot(&w0, &e1,  cosr, sinr, 0.0, e1);
+        }
+        __armas_set_at_unsafe(D, k, d0);
+        d0 = d1; e0 = e1; f = e0r; g = w0;
+        if (saves) {
+            __armas_set_at_unsafe(Cr, k-1, cosr);
+            __armas_set_at_unsafe(Sr, k-1, -sinr);
+        }
+    }
+    __armas_set_at_unsafe(D, 0, d0);
+    __armas_set_at_unsafe(E, 0, e0c);
+    return N-1;
+}
+#endif   // ! __trd_qlsweep_enhanced
+
+#if !defined(__trd_qrsweep_enhanced)
+/*
+ * \brief Implicit tridiagonal QR sweep from top to bottom.
+ *
+ *   - zx is the bulge element 
+ *
+ *   d0 | e0 | z0  .
+ *  -------------------> 1st(b)
+ *   e0 | d1 | e1  z1 
+ *  -------------------> 2nd(b)
+ *   z0 | e1 | d1  e2 
+ *   .  | z1 | e2  d2
+ *      V    V
+ *     1st  2nd
+ */
+static inline
+int __trd_qrsweep(__armas_dense_t *D, __armas_dense_t *E, __armas_dense_t *Cr,
+                  __armas_dense_t *Sr, DTYPE f0, DTYPE g0, int saves)
+{
+    DTYPE cosr, sinr, r, d0, d1, e0, e1, e0r, e0c, w0, f, g; 
+    int k;
+    int N = __armas_size(D);
+
+    f = f0; g = g0;
+    d0 = __armas_get_at_unsafe(D, 0);
+    e0 = __armas_get_at_unsafe(E, 0);
+
+    for (k = 0; k < N-1; k++) {
+        d1 = __armas_get_at_unsafe(D, k+1);
+        __gvrotg(&cosr, &sinr, &r, f, g);
+        if (k > 0)
+            __armas_set_at_unsafe(E, k-1, r);
+        __gvrot(&d0,  &e0c, cosr, sinr, d0, e0);
+        __gvrot(&e0r, &d1,  cosr, sinr, e0, d1);
+        __gvrot(&d0,  &e0r, cosr, sinr, d0, e0r);
+        __gvrot(&e0c, &d1,  cosr, sinr, e0c, d1);
+        if (k < N-2) {
+            e1 = __armas_get_at_unsafe(E, k+1);
+            __gvrot(&w0, &e1,  cosr, sinr, 0.0, e1);
+        }
+        __armas_set_at_unsafe(D, k, d0);
+        d0 = d1; e0 = e1; f = e0r; g = w0;
+        if (saves) {
+            __armas_set_at_unsafe(Cr, k, cosr);
+            __armas_set_at_unsafe(Sr, k, sinr);
+        }
+    }
+    __armas_set_at_unsafe(D, N-1, d0);
+    __armas_set_at_unsafe(E, N-2, e0r);
+    //return k > 0 ? N : 0;
+    return N-1;
+}
+#endif // ! __trd_qrsweep_enhanced
 
 /*
  * \brief Apply plane rotations from left.
@@ -262,8 +461,9 @@ DTYPE __wilkinson(DTYPE tn1, DTYPE tnn1, DTYPE tn)
     DTYPE d, tsq, sign;
     d = (tn1 - tn)/2.0;
     tsq = __HYPOT(d, tnn1);
-    sign = __SIGN(d) ? -1.0 : 1.0;
-    return tn - sign*tnn1*tnn1/(__ABS(d) + tsq);
+    return tn - __COPYSIGN((tnn1/(__ABS(d) + tsq))*tnn1, d);
+    //sign = __SIGN(d) ? -1.0 : 1.0;
+    //return tn - sign*tnn1*tnn1/(__ABS(d) + tsq);
 }
 
 
