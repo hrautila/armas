8f52f4bc163c9beb2a8832e7ac62bf04d85c1da9
Harri Rautila <harri.rautila@gmail.com>
2015-08-26 22:44:08 +0300

    Moved conditional compilation macros; added BLAS single precison macros.

--- src/inc/dtype.h
+++ src/inc/dtype.h
@@ -158,25 +158,165 @@ typedef float DTYPE;
 typedef float ABSTYPE;
 
 #define __ABSZERO (float)0.0
+#define __ABSONE  (float)1.0
 #define __ZERO    (float)0.0
 #define __ONE     (float)1.0
+#define __TWO     (float)2.0
+#define __HALF    (float)0.5
+
+#define __DATA_FORMAT "%8.1e"
+#define __PRINTABLE(a) (a)
 
 // functions from math.h
 #define __ABS   fabsf
 #define __SQRT  sqrtf
 #define __HYPOT hypotf
 #define __SIGN  signbitf
+#define __COPYSIGN  copybitf
 
-#define __SCALE  __S_SCALE
-#define __MSCALE __S_MSCALE
+// internally available functions
+#define __blk_add      __s_blk_add
+#define __blk_scale    __s_blk_scale
+#define __blk_print    __s_blk_print
+#define __vec_print    __s_vec_print
+
+// internal type dependent BLAS functions
 #define __kernel_colblk_inner  __s_kernel_colblk_inner
 #define __kernel_colwise_inner_no_scale  __s_kernel_colwise_inner_no_scale
 #define __kernel_colwise_inner_scale_c   __s_kernel_colwise_inner_scale_c
-#define __kernel_inner  __s_kernel_inner
-#define __kernel_params __s_kernel_params
+#define __kernel_inner           __s_kernel_inner
+#define __kernel_params          __s_kernel_params
+#define __gemv_recursive         __s_gemv_recursive
+#define __rank_diag              __s_rank_diag
+#define __rank2_blk              __s_rank2_blk
+#define __trmm_unb               __s_trmm_unb
+#define __trmm_blk               __s_trmm_blk
+#define __trmm_recursive         __s_trmm_recursive
+#define __trmm_blk_recursive     __s_trmm_blk_recursive
+#define __trmv_recursive         __s_trmv_recursive
+#define __trsv_recursive         __s_trsv_recursive
+#define __solve_left_unb         __s_solve_left_unb
+#define __solve_right_unb        __s_solve_right_unb
+#define __solve_blocked          __s_solve_blocked
+#define __solve_recursive        __s_solve_recursive
+#define __solve_blk_recursive    __s_solve_blk_recursive
+#define __update_trmv_unb        __s_update_trmv_unb
+#define __update_trmv_recursive  __s_update_trmv_recursive
+#define __update_trm_blk         __s_update_trm_blk
+#define __update_ger_unb         __s_update_ger_unb
+#define __update_ger_recursive   __s_update_ger_recursive
 
+// internal matrix block routines
+#define __partition_2x1           __s_partition_2x1
+#define __repartition_2x1to3x1    __s_repartition_2x1to3x1
+#define __continue_3x1to2x1       __s_continue_3x1to2x1
+#define __partition_1x2           __s_partition_1x2
+#define __repartition_1x2to1x3    __s_repartition_1x2to1x3
+#define __continue_1x3to1x2       __s_continue_1x3to1x2
+#define __partition_2x2           __s_partition_2x2
+#define __repartition_2x2to3x3    __s_repartition_2x2to3x3
+#define __continue_3x3to2x2       __s_continue_2x3to2x2
+#define __merge2x1                __s_merge2x1
+#define __merge1x2                __s_merge1x2
+
+
+// ------------------------------------------------------------------------------
+// public matrix type
 #define __armas_dense_t  armas_s_dense_t
-#define __armas_mult     armas_s_mult
+// nil matrix
+#define __nil                 ((__armas_dense_t *)0)
+
+// ------------------------------------------------------------------------------
+// public matrix functions
+#define __armas_size          armas_s_size
+#define __armas_init          armas_s_init
+#define __armas_release       armas_s_release
+#define __armas_alloc         armas_s_alloc
+#define __armas_free          armas_s_free
+#define __armas_make          armas_s_make
+#define __armas_column        armas_s_column
+#define __armas_row           armas_s_row
+#define __armas_submatrix     armas_s_submatrix
+#define __armas_subvector     armas_s_subvector
+#define __armas_submatrix_ext armas_s_submatrix_ext
+#define __armas_diag          armas_s_diag
+#define __armas_get           armas_s_get
+#define __armas_set           armas_s_set
+#define __armas_set_at        armas_s_set_at
+#define __armas_get_at        armas_s_get_at
+#define __armas_index         armas_s_index
+#define __armas_data          armas_s_data
+#define __armas_col_as_row    armas_s_col_as_row
+#define __armas_printf        armas_s_printf
+#define __armas_print         armas_s_print
+#define __armas_set_values    armas_s_set_values
+#define __armas_newcopy       armas_s_newcopy
+#define __armas_mcopy         armas_s_mcopy
+#define __armas_mscale        armas_s_mscale
+#define __armas_madd          armas_s_madd
+#define __armas_mnorm         armas_s_mnorm
+#define __armas_transpose     armas_s_transpose
+#define __armas_allclose      armas_s_allclose
+#define __armas_intolerance   armas_s_intolerance
+#define __armas_make_trm      armas_s_make_trm
+#define __armas_scale_plus    armas_s_scale_plus
+#define __armas_isvector      armas_s_isvector
+
+#define __armas_submatrix_unsafe  armas_s_submatrix_unsafe
+#define __armas_subvector_unsafe  armas_s_subvector_unsafe
+#define __armas_diag_unsafe       armas_s_diag_unsafe
+#define __armas_get_unsafe        armas_s_get_unsafe
+#define __armas_set_unsafe        armas_s_set_unsafe
+#define __armas_get_at_unsafe     armas_s_get_at_unsafe
+#define __armas_set_at_unsafe     armas_s_set_at_unsafe
+
+#define __armas_index_valid  __armas_s_index_valid
+#define __armas_real_index   __armas_s_real_index
+
+
+#define __armas_mult        armas_s_mult
+#define __armas_mult_sym    armas_s_mult_sym
+#define __armas_mult_trm    armas_s_mult_trm
+#define __armas_solve_trm   armas_s_solve_trm
+#define __armas_update_sym  armas_s_update_sym
+#define __armas_update2_sym armas_s_update2_sym
+#define __armas_update_trm  armas_s_update_trm
+// marker for blas level 3
+#define __armas_blas3   1
+
+// public functions: blas level 2, matrix-vector
+#define __armas_mvmult        armas_s_mvmult
+#define __armas_mvmult_sym    armas_s_mvmult_sym
+#define __armas_mvmult_trm    armas_s_mvmult_trm
+#define __armas_mvsolve_trm   armas_s_mvsolve_trm
+#define __armas_mvupdate      armas_s_mvupdate
+#define __armas_mvupdate_trm  armas_s_mvupdate_trm
+#define __armas_mvupdate_sym  armas_s_mvupdate_sym
+#define __armas_mvupdate2_sym armas_s_mvupdate2_sym
+// marker for blas level 2
+#define __armas_blas2   1
+
+#define __armas_mvmult_diag   armas_s_mvmult_diag
+#define __armas_mvsolve_diag  armas_s_mvsolve_diag
+
+// public functions: blas level 1, vector-vector
+#define __armas_nrm2    armas_s_nrm2
+#define __armas_asum    armas_s_asum
+#define __armas_scale   armas_s_scale
+#define __armas_iamax   armas_s_iamax
+#define __armas_dot     armas_s_dot
+#define __armas_axpy    armas_s_axpy
+#define __armas_axpby   armas_s_axpby
+#define __armas_swap    armas_s_swap
+#define __armas_copy    armas_s_copy
+// marker for blas level 1
+#define __armas_blas1   1
+// 
+#define __armas_invscale armas_s_invscale
+#define __armas_sum      armas_s_sum
+#define __armas_amax     armas_s_amax
+#define __armas_add      armas_s_add
+#define __armas_iamin    armas_s_iamin
 
 #else  // default is double precision float (FLOAT64)
 /* ---------------------------------------------------------------------------
@@ -375,32 +515,6 @@ typedef double ABSTYPE;
 
 #if defined(__armas_blas1) && defined(__armas_blas2) && defined(__armas_blas3)
 #define __armas_blas 1
-#endif
-
-#if EXT_PRECISION
-
-// If <flag> expression TRUE, call <func> and return with it's return value
-#define IF_EXPR(flag, func)			\
-  do {						\
-    if (flag) {					\
-      return func;				\
-    }						\
-  } while(0)
-
-// If <flag> expression TRUE, call <func> and return with it's return value
-#define IF_EXPR2(flag, rval, func)		\
-  do {						\
-    if (flag) {					\
-      func; return rval;			\
-    }						\
-  } while(0)
-
-#else
-
-#define IF_EXPR(flag, func) 
-
-#define IF_EXPR2(flag, rval func) 
-
 #endif
 
 
