ffea92a7931fa271e2a0077a4f7f96a5b49e4879
Harri Rautila <harri.rautila@gmail.com>
2015-09-28 22:51:34 +0300

    Added conditional compilation for __x86_64__ functions.

--- src/inc/debug.h
+++ src/inc/debug.h
@@ -10,17 +10,25 @@
 
 #include <math.h>
 
+#if defined(__DEBUG__)
+
 #if defined(COMPLEX128)
 static inline void __check_nan(double complex val) {}
+#ifdef __x86_64__
 static inline void __check_mm256(__m256d val) {}
+#endif
 
 #elif defined(COMPLEX64)
 static inline void __check_nan(float complex val) {}
+#ifdef __x86_64__
 static inline void __check_mm256(__m256d val) {}
+#endif
 
 #elif defined(FLOAT32)
 static inline void __check_nan(float val) {}
+#ifdef __x86_64__
 static inline void __check_mm256(__m256d val) {}
+#endif
 
 #else // FLOAT(64)
 static inline void __check_nan(double val) {
@@ -30,19 +38,22 @@ static inline void __check_nan(double val) {
     *ptr = val;
 }
 
+#ifdef __x86_64__
 static inline void __check_mm256(__m256d val) {
   __check_nan(val[0]);
   __check_nan(val[1]);
   __check_nan(val[2]);
   __check_nan(val[3]);
 }
+#endif
 
 #endif
 
-#if defined(__DEBUG__)
 #define CHECK_NAN(val)  __check_nan(val)
 
+#ifdef __x86_64__
 #define CHECK_MM256(val) __check_mm256(val)
+#endif
 
 #define IFERROR(exp) do { \
   int _e = (exp); \
