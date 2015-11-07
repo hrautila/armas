d4d611b7dcafd71332cb792c0cabe4c14efe4f89
Harri Rautila <harri.rautila@gmail.com>
2015-11-08 00:29:43 +0200

    Thread support
    Support for all threading models.

--- src/blas3/syr2k.c
+++ src/blas3/syr2k.c
@@ -26,7 +26,7 @@
 
 #include "internal.h"
 #include "matrix.h"
-
+#include "scheduler.h"
 
 // SYR2K;
 //   C = alpha*A*B.T + alpha*B*A.T + beta*C  
@@ -157,22 +157,228 @@ void __rank2_blk(mdata_t *C, const mdata_t *A, const mdata_t *B,
   }
 }
 
+#if defined(ENABLE_THREADS)
+
+extern
+void __update_trm_blocked(mdata_t *C, const mdata_t *A, const mdata_t *B,
+                          DTYPE alpha, DTYPE beta, int flags, int P, int S, int L, 
+                          int R, int E, int KB, int NB, int MB, armas_cbuf_t *cbuf);
 
 static
-int __rank2_threaded(int blk, int nblk, __armas_dense_t *C,
+void *__compute_recursive(void *arg)
+{
+  kernel_param_t *kp = ((block_args_t *)arg)->kp;
+  armas_cbuf_t *cbuf  = ((block_args_t *)arg)->cbuf;
+
+  int flags = kp->flags & ARMAS_TRANS ? ARMAS_TRANSA : ARMAS_TRANSB;
+  flags |= kp->flags & ARMAS_UPPER ? ARMAS_UPPER : ARMAS_LOWER;
+  
+  // C = beta*C + alpha*A*B.T or C = beta*C + alpha*A.T*B
+  __update_trm_blocked(&kp->C, &kp->A, &kp->B, kp->alpha, kp->beta, flags, 
+                       kp->K, kp->S, kp->L, kp->R, kp->E, kp->MB, kp->NB, kp->KB, cbuf);
+  // C = C + alpha*B*A.T or C = C + alpha*B.T*A
+  __update_trm_blocked(&kp->C, &kp->B, &kp->A, kp->alpha, __ONE, flags, 
+                       kp->K, kp->S, kp->L, kp->R, kp->E, kp->MB, kp->NB, kp->KB, cbuf);
+  return (void *)0;
+}
+
+static 
+int __rank2_recursive(int blk, int nblk, __armas_dense_t *C,
+                      const __armas_dense_t *A, const __armas_dense_t *B,
+                      DTYPE alpha, DTYPE beta, int flags, armas_conf_t *conf)
+{
+  int rs, re, cs, ce, err;
+  mdata_t *_C, C0, A0, B0;
+  const mdata_t *_A, *_B; 
+  pthread_t th;
+  kernel_param_t kp;
+  armas_cbuf_t cbuf;
+  block_args_t args = (block_args_t){&kp, &cbuf};
+
+  if (flags & ARMAS_UPPER) {
+    rs = __block_index4(blk, nblk, C->rows);
+    re = __block_index4(blk+1, nblk, C->rows);
+    cs = rs;
+    ce = C->cols;
+  } else {
+    cs = __block_index4(blk, nblk, C->cols);
+    ce = __block_index4(blk+1, nblk, C->cols);
+    rs = cs;
+    re = C->rows;
+  }
+
+  _C = (mdata_t *)C;
+  _A = (const mdata_t *)A;
+  _B = (const mdata_t *)B;
+
+  int K = flags & ARMAS_TRANS ? A->rows : A->cols;
+
+  // shift the start point to top-left corner [rs,cs] of this block. Block size
+  //   UPPER:  [rows/nblk-rs, cols-ce] 
+  //   LOWER:  [cols/nblk-cs, rows-rs] 
+  __subblock(&C0, _C, rs, cs);
+  __subblock(&A0, _A, (flags & ARMAS_TRANS ? 0 : rs), (flags & ARMAS_TRANS ? rs : 0));
+  __subblock(&B0, _B, (flags & ARMAS_TRANS ? 0 : rs), (flags & ARMAS_TRANS ? rs : 0));
+
+  if (blk == nblk-1) {
+    armas_cbuf_init(&cbuf, conf->cmem, conf->l1mem);
+    
+    __rank2_blk(&C0, &A0, &B0, alpha, beta, flags, K, 0, re-rs,
+                conf->kb, conf->nb, conf->mb, &cbuf);
+
+    armas_cbuf_release(&cbuf);
+    return 0;
+  }
+
+  __kernel_params(&kp, &C0, &A0, &B0, alpha, beta, flags, K, 0, ce-cs,
+                  0, re-rs, conf->kb, conf->nb, conf->mb, conf->optflags);
+
+  // create new thread to compute this block
+  armas_cbuf_init(&cbuf, conf->cmem, conf->l1mem);
+  err = pthread_create(&th, NULL, __compute_recursive, &args);
+  if (err) {
+    conf->error = -err;
+    armas_cbuf_release(&cbuf);
+    return -1;
+  }
+  // recursively invoke next block
+  err = __rank2_recursive(blk+1, nblk, C, A, B, alpha, beta, flags, conf);
+  // wait for this block to finish
+  pthread_join(th, NULL);
+  armas_cbuf_release(&cbuf);
+  return 0;
+
+}
+
+static
+void *__compute_block2(void *arg, armas_cbuf_t *cbuf)
+{
+  kernel_param_t *kp = (kernel_param_t *)arg;
+
+  int flags = kp->flags & ARMAS_TRANS ? ARMAS_TRANSA : ARMAS_TRANSB;
+  flags |= kp->flags & ARMAS_UPPER ? ARMAS_UPPER : ARMAS_LOWER;
+  
+  if (kp->optflags & ARMAS_BLAS_BLOCKED) {
+    // C = beta*C + alpha*A*B.T || C = beta*C + alpha*A.T*B
+    __update_trm_blocked(&kp->C, &kp->A, &kp->B, kp->alpha, kp->beta, flags, 
+                         kp->K, kp->S, kp->L, kp->R, kp->E, kp->MB, kp->NB, kp->KB, cbuf);
+    // C = C + alpha*B*A.T || C = C + alpha*B.T*A
+    __update_trm_blocked(&kp->C, &kp->B, &kp->A, kp->alpha, __ONE, flags, 
+                         kp->K, kp->S, kp->L, kp->R, kp->E, kp->MB, kp->NB, kp->KB, cbuf);
+  } else {
+    // tiled
+    //printf("..tiled: R,S=%d,%d, E,L=%d,%d\n", kp->R, kp->S,  kp->E, kp->L);
+    if (kp->S == kp->R && kp->L == kp->E) {
+      // diagonal block; 
+      __update_trm_blocked(&kp->C, &kp->A, &kp->B, kp->alpha, kp->beta, flags, 
+                           kp->K, kp->S, kp->L, kp->R, kp->E, kp->MB, kp->NB, kp->KB, cbuf);
+
+      __update_trm_blocked(&kp->C, &kp->B, &kp->A, kp->alpha, __ONE, flags, 
+                           kp->K, kp->S, kp->L, kp->R, kp->E, kp->MB, kp->NB, kp->KB, cbuf);
+    } else {
+      // square block;
+      __kernel_inner(&kp->C, &kp->A, &kp->B, kp->alpha, kp->beta, flags,
+                     kp->K, kp->S, kp->L, kp->R, kp->E, kp->KB, kp->NB, kp->MB, cbuf);
+
+      __kernel_inner(&kp->C, &kp->B, &kp->A, kp->alpha, __ONE, flags,
+                     kp->K, kp->S, kp->L, kp->R, kp->E, kp->KB, kp->NB, kp->MB, cbuf);
+
+    }
+  }
+  return (void *)0;
+}
+
+static 
+int __rank2_schedule(int nblk, __armas_dense_t *C,
                      const __armas_dense_t *A, const __armas_dense_t *B,
                      DTYPE alpha, DTYPE beta, int flags, armas_conf_t *conf)
 {
-  int K = flags & ARMAS_TRANSA ? A->rows : A->cols;
-  armas_cbuf_t *cbuf = armas_cbuf_get(conf);
+  int rN, cN, i, j, iR, iE, jS, jL, k, nT, K;
+  blas_task_t *tasks;
+  mdata_t *_C;
+  const mdata_t *_A, *_B;
+  armas_counter_t ready;
 
-  // no threading support yet; execute like single threaded
-  __rank2_blk((mdata_t *)C, (const mdata_t *)A, (const mdata_t *)B, alpha, beta,
-              flags, K, 0, C->rows, conf->kb, conf->nb, conf->mb, cbuf);
+  _C = (mdata_t*)C;
+  _A = (const mdata_t *)A;
+  _B = (const mdata_t *)B;
+
+  K = flags & ARMAS_TRANS ? A->rows : A->cols;
+
+  // number of tasks
+  nT = nblk; rN = cN = 0;
+  if (conf->optflags & ARMAS_BLAS_TILED) {
+    nT = blocking(C->rows, C->cols, conf->wb, &rN, &cN);
+    // substract the off-diagonal tile count
+    nT -= ((rN - 1)*cN)/2;
+  }
+
+  tasks = (blas_task_t *)calloc(nT, sizeof(blas_task_t));
+  if (! tasks) {
+    conf->error = ARMAS_EMEMORY;
+    return -1;
+  }
+  armas_counter_init(&ready, nT);
+  k = 0; 
+
+  if (conf->optflags & ARMAS_BLAS_BLOCKED) {
+    // compute in nblk blocks (row stripes for upper/column stripes for lower)
+    iR = 0; iE = C->rows;
+    jS = 0; jL = C->cols;
+    for (j = 0; j < nblk; j++) {
+      if (flags & ARMAS_UPPER) {
+        iR = __block_index4(j,   nblk, C->rows);
+        iE = __block_index4(j+1, nblk, C->rows);
+        jS = iR;
+      } else {
+        jS = __block_index4(j,   nblk, C->cols);
+        jL = __block_index4(j+1, nblk, C->cols);
+        iR = jS;
+      }
+      __kernel_params(&tasks[k].kp, _C, _A, _B, alpha, beta, flags, K, jS, jL, iR, iE,
+                      conf->kb, conf->nb, conf->mb, conf->optflags);
+      // init task and schedule
+      armas_task2_init(&tasks[k].t, k, __compute_block2, &tasks[k].kp, &ready);
+      armas_schedule(&tasks[k].t);
+      k++;
+    }
+  } else {
+    // compute in tiles of wb x wb; C is square matrix --> cN == rN
+    for (j = 0; j < cN; j++) {
+      jS = block_index(j,   cN, conf->wb, C->cols);
+      jL = block_index(j+1, cN, conf->wb, C->cols);
+      for (i = j; i < rN; i++) {
+        iR = block_index(i,   rN, conf->wb, C->rows);
+        iE = block_index(i+1, rN, conf->wb, C->rows);
+        if (flags & ARMAS_UPPER) {
+          __kernel_params(&tasks[k].kp, _C, _A, _B, alpha, beta, flags,
+                          K, iR, iE, jS, jL, conf->kb, conf->nb, conf->mb, conf->optflags);
+        } else {
+          __kernel_params(&tasks[k].kp, _C, _A, _B, alpha, beta, flags,
+                          K, jS, jL, iR, iE, conf->kb, conf->nb, conf->mb, conf->optflags);
+        }
+
+        // init task and schedule
+        armas_task2_init(&tasks[k].t, k, __compute_block2, &tasks[k].kp, &ready);
+        armas_schedule(&tasks[k].t);
+        k++;
+      }
+    }
+  }
+  assert(k == nT);
+  // wait for tasks to finish
+  armas_counter_wait(&ready);
+  // 1. check that task worker count is zero on all tasks
+  int refcnt = 0;
+  for (i = 0; i < nT; i++) {
+    refcnt += tasks[i].t.wcnt;
+  }
+  assert(refcnt == 0);
+  // release task memory
+  free(tasks);
   return 0;
 }
-
-
+#endif // ENABLE_THREADS
 
 /**
  * @brief Symmetric matrix rank-2k update
@@ -208,16 +414,33 @@ int __armas_update2_sym(__armas_dense_t *C,
   if (!conf)
     conf = armas_conf_default();
 
+  int ok = A->rows == B->rows && A->cols == B->cols && C->rows == C->cols;
+  switch (flags & ARMAS_TRANS) {
+  case ARMAS_TRANS:
+    ok = ok && C->rows == A->cols;
+    break;
+  default:
+    ok = ok && C->rows == A->rows;
+    break;
+  }
+  if (!ok) {
+    conf->error = ARMAS_ESIZE;
+    return -1;
+  }
+  
 #if defined(ENABLE_THREADS)
   long nproc = armas_use_nproc(__armas_size(C), conf);
-  return __rank2_threaded(0, nproc, C, A, B, alpha, beta, flags, conf);
+  if (conf->optflags & (ARMAS_BLAS_BLOCKED|ARMAS_BLAS_TILED)) {
+    return __rank2_schedule(nproc, C, A, B, alpha, beta, flags, conf);
+  }
+  return __rank2_recursive(0, nproc, C, A, B, alpha, beta, flags, conf);
 #else
   mdata_t *_C = (mdata_t*)C;
   const mdata_t *_A = (const mdata_t *)A;
   const mdata_t *_B = (const mdata_t *)B;
   armas_cbuf_t *cbuf = armas_cbuf_get(conf);
 
-  int K = flags & ARMAS_TRANSA ? A->rows : A->cols;
+  int K = flags & ARMAS_TRANS ? A->rows : A->cols;
 
   // if only one thread, just do it
   __rank2_blk(_C, _A, _B, alpha, beta, flags, K, 0, C->rows,
