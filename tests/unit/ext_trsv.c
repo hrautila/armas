a51bf17575357488b4cbce309c5f1f2b32ffa8ab
Harri Rautila <harri.rautila@gmail.com>
2015-11-13 23:37:22 +0200

    Unit diagonal tests.

--- tests/unit/ext_trsv.c
+++ tests/unit/ext_trsv.c
@@ -6,59 +6,61 @@
 #include <float.h>
 
 #include "testing.h"
-//#include <armas/dmatrix.h>
-//#include "helper.h"
 
 /*
- *  Generate set matrices such that Y = A * X
- *
- *   (D0)    (q0 q1 q2 q3)  (z0)
- *   (z1) =  ( 0  1  0  0)  (z1)
- *   (z2)    ( 0  0  1  0)  (z2)
- *   (z3)    ( 0  0  0  1)  (z3)
- *
- *   D0 = (q0 q1 q2 q3)*(z0 z1 z2 z3).T   (dot product)
+ * See comments in ext_trsm.c
  */
-// C = A*B  => B = A.-1*C
 void ep_gentrsv(double *dot, double *tcond,
                 __Matrix *A, __Matrix *X, __Matrix *Y,
                 double cond, int flags)
 {
-    __Matrix R0, C0, R1, C1, D;
-    int k, tk;
-    int right = flags & ARMAS_RIGHT;
+  __Matrix R0, D, Rx, Cx;
+  int tk;
 
-    // make A identity
-    matrix_set_values(A, zero, 0);
-    matrix_diag(&D, A, 0);
-    matrix_set_values(&D, one, 0);
+  // make A identity
+  matrix_init(&R0, 0, 0);
+  matrix_set_values(A, zero, 0);
+  matrix_diag(&D, A, 0);
+  matrix_set_values(&D, one, 0);
     
-    switch (flags & (ARMAS_UPPER|ARMAS_LOWER|ARMAS_TRANS)) {
-    case ARMAS_LOWER|ARMAS_TRANS:
-        matrix_column(&R0, A, 0);
-        tk = 0;
-        break;
-    case ARMAS_LOWER:
-        matrix_row(&R0, A, A->rows-1);
-        tk = X->rows - 1;
-        break;
-    case ARMAS_UPPER|ARMAS_TRANS:
-        matrix_column(&R0, A, A->cols-1);
-        tk = X->rows - 1;
-        break;
-    case ARMAS_UPPER:
-    default:
-        matrix_row(&R0, A, 0);
-        tk = 0;
-        break;
-    }
+  switch (flags & (ARMAS_UPPER|ARMAS_LOWER|ARMAS_TRANS)) {
+  case ARMAS_LOWER|ARMAS_TRANS:
+    matrix_column(&R0, A, 0);
+    matrix_subvector(&Rx, &R0, 1, matrix_size(&R0)-1);
+    tk = 0;
+    break;
+  case ARMAS_LOWER:
+    matrix_row(&R0, A, A->rows-1);
+    matrix_subvector(&Rx, &R0, 0, matrix_size(&R0)-1);
+    tk = X->rows - 1;
+    break;
+  case ARMAS_UPPER|ARMAS_TRANS:
+    matrix_column(&R0, A, A->cols-1);
+    matrix_subvector(&Rx, &R0, 0, matrix_size(&R0)-1);
+    tk = X->rows - 1;
+    break;
+  case ARMAS_UPPER:
+  default:
+    matrix_row(&R0, A, 0);
+    matrix_subvector(&Rx, &R0, 1, matrix_size(&R0)-1);
+    tk = 0;
+    break;
+  }
 
-    // generate dot product ...
-    ep_gendot(dot, tcond, &R0, X, cond);
-
-    // make result vector
-    matrix_mcopy(Y, X);
-    matrix_set_at(Y, tk, *dot);
+  // generate dot product ...
+  matrix_subvector(&Cx, X, tk == 0 ? 1 : 0, matrix_size(X)-1);
+  ep_gendot(dot, tcond, &Rx, &Cx, cond);
+  if (flags & ARMAS_UNIT) {
+    matrix_set(A, tk, tk, 1.0);
+    matrix_set_at(X, tk, *dot);
+  } else {
+    matrix_set(A, tk, tk, *dot);
+    matrix_set_at(X, tk, 1.0);
+  }
+  
+  // make result vector
+  matrix_mcopy(Y, X);
+  matrix_set_at(Y, tk, 2.0*(*dot));
 }
 
 // ne = norm1 of exact; re = ||exact-result||/||exact||
@@ -66,22 +68,18 @@ void compute(double *ne, double *re,
              __Matrix *B, __Matrix *A, __Matrix *C,
              int flags, int prec, int verbose, armas_conf_t *conf)
 {
-  int bits = 0;
-
-
   // B = A*B
   matrix_mvsolve_trm(B, A, 1.0, flags, conf);
   if (verbose > 1 && A->rows < 10) {
-    //printf("exc(A*B):\n"); matrix_printf(stdout, "%13e", C);
     printf("A.-1*B:\n"); matrix_printf(stdout, "%13e", B);
   }
-  *re = rel_error(ne, B, C, ARMAS_NORM_ONE, ARMAS_NONE, conf);
+  *re = rel_error(ne, B, C, ARMAS_NORM_INF, ARMAS_NONE, conf);
 }
 
-int test(char *name, int N, int K, int flags, int verbose, int prec, double cwant, armas_conf_t *conf)
+int test(char *name, int N, int flags, int verbose, int prec, double cwant, armas_conf_t *conf)
 {
-  __Matrix C, Ct, T, B0, A, B, Ce;
-  double dot, cond, m_c, m_one;
+  __Matrix B0, A, B, Ce;
+  double dot, cond, m_c, m_rel;
   int ok;
 
 
@@ -94,19 +92,17 @@ int test(char *name, int N, int K, int flags, int verbose, int prec, double cwan
 
   if (verbose > 1 && N < 10) {
     printf("A:\n"); matrix_printf(stdout, "%13e", &A);
-    if (K < 10) {
-      printf("B0:\n"); matrix_printf(stdout, "%13e", &B0);
-      printf("B:\n"); matrix_printf(stdout, "%13e", &B);
-    }
+    printf("B0:\n"); matrix_printf(stdout, "%13e", &B0);
+    printf("B:\n"); matrix_printf(stdout, "%13e", &B);
   }
 
   // extended precision computations
 
   // 1. A*B
-  compute(&m_c, &m_one, &B, &A, &B0, flags, prec, verbose, conf);
+  compute(&m_c, &m_rel, &B, &A, &B0, flags, prec, verbose, conf);
 
-  ok = m_one < N*DBL_EPSILON;
-  printf("%-4s: %s rel.error %e [%e]\n",  PASS(ok), name, m_one, m_c);
+  ok = m_rel < N*_EPS;
+  printf("%-4s: %s rel.error %e [%e]\n",  PASS(ok), name, m_rel, m_c);
   if (!ok && N < 10) {
     printf("B-Ce:\n"); matrix_printf(stdout, "%13e", &B);
   }
@@ -126,24 +122,20 @@ int main(int argc, char **argv)
 {
 
   armas_conf_t conf;
-  __Matrix C, Ct, T, B0, A, B, Ce;
 
   int ok, opt;
-  int N = 33;
-  int M = 33;
-  int K = 33;
+  int N = 121;
   int fails = 0;
   int normal_prec = 0;
-  int e0, e1;
   int prec = 200;
   int verbose = 0;
   int flags = ARMAS_UPPER;
-  int all = 0;
+  int all = 1;
+  int unit = 0;
   int naive = 0;
-  double cwant = 1e14; // wanted condition number
-  double dot, cond, m_one_t, m_one, m_c;
+  double cwant = 1.0/_EPS; // wanted condition number
 
-  while ((opt = getopt(argc, argv, "C:p:vnASLRT")) != -1) {
+  while ((opt = getopt(argc, argv, "C:p:vnSsLTU")) != -1) {
     switch (opt) {
     case 'C':
       cwant = strtod(optarg, (char **)0);
@@ -151,7 +143,7 @@ int main(int argc, char **argv)
     case 'p':
       prec = atoi(optarg);
       break;
-    case 'S':
+    case 's':
       normal_prec = 1;
       break;
     case 'n':
@@ -160,37 +152,30 @@ int main(int argc, char **argv)
     case 'v':
       verbose += 1;
       break;
-    case 'A':
-      all = 1;
+    case 'S':
+      all = 0;
       break;
     case 'L':
       flags &= ~ARMAS_UPPER;
       flags |= ARMAS_LOWER;
       break;
-    case 'R':
-      flags &= ~ARMAS_LEFT;
-      flags |= ARMAS_RIGHT;
-      break;
     case 'T':
       flags |= ARMAS_TRANS;
       break;
+    case 'U':
+      flags |= ARMAS_UNIT;
+      unit  |= ARMAS_UNIT;
+      break;
     default:
-      fprintf(stderr, "usage: ext_gemm [-p bits -C cond -v -SLRT] [size]\n");
+      fprintf(stderr, "usage: ext_trsv [-p bits -C cond -v -SLRTU] [size]\n");
       exit(1);
     }
   }
     
-  if (optind < argc-1) {
+  if (optind < argc) {
     N = atoi(argv[optind]);
-    K = atoi(argv[optind+1]);
-  } else if (optind < argc) {
-    N = atoi(argv[optind]);
-    M = K = N;
   }
   
-  if (verbose > 1) {
-    printf("flags = 0x%X\n", flags);
-  }
   conf = *armas_conf_default();
   if (!normal_prec)
     conf.optflags |= ARMAS_OEXTPREC;
@@ -199,16 +184,25 @@ int main(int argc, char **argv)
 
   if (!all) {
     //printf("conf .mb, .nb, .kb: %d, %d, %d\n", conf.mb, conf.nb, conf.kb);
-    ok = test("single", N, K, flags, verbose, prec, cwant, &conf);
+    ok = test("single", N, flags, verbose, prec, cwant, &conf);
     fails += 1 - ok;
   } else {
-    ok = test("u(A)*B  ", N, K, ARMAS_UPPER, verbose, prec, cwant, &conf);
+    ok = test("N: u(A)*B  ", N, ARMAS_UPPER, verbose, prec, cwant, &conf);
     fails += 1 - ok;
-    ok = test("l(A)*B  ", N, K, ARMAS_LOWER, verbose, prec, cwant, &conf);
+    ok = test("N: l(A)*B  ", N, ARMAS_LOWER, verbose, prec, cwant, &conf);
     fails += 1 - ok;
-    ok = test("u(A.T)*B", N, K, ARMAS_UPPER|ARMAS_TRANS, verbose, prec, cwant, &conf);
+    ok = test("N: u(A.T)*B", N, ARMAS_UPPER|ARMAS_TRANS, verbose, prec, cwant, &conf);
     fails += 1 - ok;
-    ok = test("l(A.T)*B", N, K, ARMAS_LOWER|ARMAS_TRANS, verbose, prec, cwant, &conf);
+    ok = test("N: l(A.T)*B", N, ARMAS_LOWER|ARMAS_TRANS, verbose, prec, cwant, &conf);
+    fails += 1 - ok;
+
+    ok = test("U: u(A)*B  ", N, ARMAS_UNIT|ARMAS_UPPER, verbose, prec, cwant, &conf);
+    fails += 1 - ok;
+    ok = test("U: l(A)*B  ", N, ARMAS_UNIT|ARMAS_LOWER, verbose, prec, cwant, &conf);
+    fails += 1 - ok;
+    ok = test("U: u(A.T)*B", N, ARMAS_UNIT|ARMAS_UPPER|ARMAS_TRANS, verbose, prec, cwant, &conf);
+    fails += 1 - ok;
+    ok = test("U: l(A.T)*B", N, ARMAS_UNIT|ARMAS_LOWER|ARMAS_TRANS, verbose, prec, cwant, &conf);
     fails += 1 - ok;
   }
 
