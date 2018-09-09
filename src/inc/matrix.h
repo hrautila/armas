edb6cd9fe2c21d1505f396c79d46d75d480c6e74
Harri Rautila <harri.rautila@gmail.com>
2018-09-10 01:07:08 +0300

    Updated interfaces.

--- src/inc/matrix.h
+++ src/inc/matrix.h
@@ -570,7 +570,7 @@ extern int armas_x_bdbuild_w(armas_x_dense_t *A, const armas_x_dense_t *tau,
                              int K, int flags, armas_wbuf_t *wrk, armas_conf_t *conf);
 extern int armas_x_bdmult(armas_x_dense_t *B, armas_x_dense_t *A, armas_x_dense_t *tau,
                           armas_x_dense_t *W, int flags, armas_conf_t *conf);
-extern int armas_x_bdmult_w(armas_x_dense_t *B, armas_x_dense_t *A, armas_x_dense_t *tau,
+extern int armas_x_bdmult_w(armas_x_dense_t *B, const armas_x_dense_t *A, const armas_x_dense_t *tau,
                             int flags, armas_wbuf_t *w, armas_conf_t *conf);
 extern int armas_x_bdreduce_work(armas_x_dense_t *A, armas_conf_t *conf);
 extern int armas_x_bdmult_work(armas_x_dense_t *A, int flags, armas_conf_t *conf);
@@ -736,7 +736,7 @@ extern int armas_x_trdeigen(armas_x_dense_t *D, armas_x_dense_t *E, armas_x_dens
 extern int armas_x_trdeigen_w(armas_x_dense_t *D, armas_x_dense_t *E, armas_x_dense_t *V,
                               int flags, armas_wbuf_t *wb, armas_conf_t *conf);
 extern int armas_x_trdbisect(armas_x_dense_t *Y, armas_x_dense_t *D, armas_x_dense_t *E,
-                             armas_x_eigen_parameter_t *params,  armas_conf_t *conf);
+                             const armas_x_eigen_parameter_t *params,  armas_conf_t *conf);
 // Secular functions solvers
 extern int armas_x_trdsec_solve(armas_x_dense_t *y, armas_x_dense_t *d,
                                 armas_x_dense_t *z, armas_x_dense_t *delta, DTYPE rho,
@@ -777,7 +777,8 @@ extern int armas_x_eigen_sym_w(armas_x_dense_t *D, armas_x_dense_t *A,
                                int flags, armas_wbuf_t *wrk, armas_conf_t *conf); 
 extern int armas_x_eigen_sym_selected(armas_x_dense_t *D, armas_x_dense_t *A, armas_x_dense_t *W, 
                                       armas_x_eigen_parameter_t *params, int flags, armas_conf_t *conf);
-extern int armas_x_eigen_sym_selected_w(armas_x_dense_t *D, armas_x_dense_t *A, armas_x_eigen_parameter_t *params,
+extern int armas_x_eigen_sym_selected_w(armas_x_dense_t *D, armas_x_dense_t *A,
+                                        const armas_x_eigen_parameter_t *params,
                                         int flags, armas_wbuf_t *wrk, armas_conf_t *conf);
 
 // DQDS
