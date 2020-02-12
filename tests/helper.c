0ca175815d69fd16181c01e37d677e46742a334a
Harri Rautila <harri.rautila@gmail.com>
2020-02-13 00:55:10 +0200

    Cleanup testing helper functions.

--- tests/helper.c
+++ tests/helper.c
@@ -6,296 +6,280 @@
 #include <sys/time.h>
 #include <float.h>
 
-#if defined(FLOAT32)
-#include "armas/smatrix.h"
-typedef float DTYPE;
-typedef armas_s_dense_t armas_x_dense_t;
-#define EPSILON FLT_EPSILON
-#define FABS fabsf
-#define armas_x_is_vector    armas_s_isvector
-#define armas_x_axpy         armas_s_axpy
-#define armas_x_scale_plus armas_s_scale_plus
-#define armas_x_mnorm        armas_s_mnorm
-#define armas_x_make         armas_s_make
-#define armas_x_size         armas_s_size
-#define armas_x_data         armas_s_data
+#include "testing.h"
 
-#else
-#include "armas/dmatrix.h"
-typedef double DTYPE;
-typedef armas_d_dense_t armas_x_dense_t;
-
-#define EPSILON DBL_EPSILON
-#define FABS fabs
-#define armas_x_is_vector    armas_d_isvector
-#define armas_x_axpy         armas_d_axpy
-#define armas_x_scale_plus armas_d_scale_plus
-#define armas_x_mnorm        armas_d_mnorm
-#define armas_x_make         armas_d_make
-#define armas_x_size         armas_d_size
-#define armas_x_data         armas_d_data
-
-#endif
-
-static inline int max(int a, int b)
+static inline
+int max(int a, int b)
 {
-  return a > b ? a : b;
+    return a > b ? a : b;
 }
 
-static inline int min(int a, int b)
+static inline
+int min(int a, int b)
 {
-  return a < b ? a : b;
+    return a < b ? a : b;
 }
 
 #define FLUSH_SIZE 2048*2048
-double flush() {
-  static double *data = (double *)0;
-  int i;
-  double t = 0.0;
-  if (!data) {
-    data = (double *)malloc(FLUSH_SIZE*sizeof(double));
-  }
+double flush()
+{
+    static double *data = (double *) 0;
+    int i;
+    double t = 0.0;
+    if (!data) {
+        data = (double *) malloc(FLUSH_SIZE * sizeof(double));
+    }
 
-  for (i = 0; i < FLUSH_SIZE; i++) {
-    data[i] = 1e-12;
-  }
-  for (i = 0; i < FLUSH_SIZE; i++) {
-    t += data[i];
-  }
-  if (t < 10.0) {
-    t = 0.0;
-  }
-  return t;
+    for (i = 0; i < FLUSH_SIZE; i++) {
+        data[i] = 1e-12;
+    }
+    for (i = 0; i < FLUSH_SIZE; i++) {
+        t += data[i];
+    }
+    if (t < 10.0) {
+        t = 0.0;
+    }
+    return t;
 }
 
-double time_msec() {
-  struct timeval tv;
-  gettimeofday(&tv, 0);
-  return (1e+6*tv.tv_sec + tv.tv_usec)/1000.0;
+double time_msec()
+{
+    struct timeval tv;
+    gettimeofday(&tv, 0);
+    return (1e+6 * tv.tv_sec + tv.tv_usec) / 1000.0;
 }
 
 double gflops(double ms, int64_t count)
 {
-  return (count/ms)*1e-6;
+    return (count / ms) * 1e-6;
 }
 
-DTYPE zero(int i, int j) {
-  return 0.0;
+DTYPE zero(int i, int j)
+{
+    return 0.0;
 }
 
-DTYPE one(int i, int j) {
-  return 1.0;
+DTYPE one(int i, int j)
+{
+    return 1.0;
 }
 
-DTYPE rowno(int i, int j) {
-  return (i+1)*1.0;
+DTYPE rowno(int i, int j)
+{
+    return (i + 1) * 1.0;
 }
 
-DTYPE colno(int i, int j) {
-  return (j+1)*1.0;
+DTYPE colno(int i, int j)
+{
+    return (j + 1) * 1.0;
 }
 
-DTYPE zeromean(int i, int j) {
-  static int init = 0;
-  if (!init) {
-    srand48((long)time(0));
-    init = 1;
-  }
-  return (DTYPE)(drand48() - 0.5);
+DTYPE zeromean(int i, int j)
+{
+    static int init = 0;
+    if (!init) {
+        srand48((long) time(0));
+        init = 1;
+    }
+    return (DTYPE) (drand48() - 0.5);
 }
 
 DTYPE unitrand(int i, int j)
 {
-  static int init = 0;
-  if (!init) {
-    srand48((long)time(0));
-    init = 1;
-  }
-  return (DTYPE)drand48();
+    static int init = 0;
+    if (!init) {
+        srand48((long) time(0));
+        init = 1;
+    }
+    return (DTYPE) drand48();
 }
 
 // std random variable from unit random with box-muller transformation
 DTYPE stdrand(int i, int j)
-{ 
-  double s, u, v;
-  static int init = 0;
-  if (!init) {
-    srand48((long)time(0));
-    init = 1;
-  }
-  do {
-    u = 2.0*drand48() - 1.0;
-    v = 2.0*drand48() - 1.0;
-    s = u*u + v*v;
-  } while (s == 0.0 || s >= 1.0);
-  return (DTYPE)(u * sqrt(-2.0*log(s)/s));
+{
+    double s, u, v;
+    static int init = 0;
+    if (!init) {
+        srand48((long) time(0));
+        init = 1;
+    }
+    do {
+        u = 2.0 * drand48() - 1.0;
+        v = 2.0 * drand48() - 1.0;
+        s = u * u + v * v;
+    } while (s == 0.0 || s >= 1.0);
+    return (DTYPE) (u * sqrt(-2.0 * log(s) / s));
 }
 
 int isOK(DTYPE nrm, int N)
 {
-  //int nk = (int64_t)(FABS(nrm/EPSILON));
-  //return nrm != 0.0 && nk < (int64_t)N;
-  // if exactly zero then something suspect
-  return nrm != 0.0 && FABS(nrm) < (DTYPE)N*EPSILON;
+    //int nk = (int64_t)(FABS(nrm/EPSILON));
+    //return nrm != 0.0 && nk < (int64_t)N;
+    // if exactly zero then something suspect
+    return nrm != 0.0 && ABS(nrm) < (DTYPE) N *EPS;
 }
 
 int isFINE(DTYPE nrm, DTYPE tol)
 {
-  // if exactly zero then something suspect
-  return nrm != 0.0 && FABS(nrm) < FABS(tol);
+    // if exactly zero then something suspect
+    return nrm != 0.0 && ABS(nrm) < ABS(tol);
 }
 
 int in_tolerance(double a, double b)
 {
-  static const double RTOL = 1.0000000000000001e-05;
-  static const double ATOL = 1e-8;
+    static const double RTOL = 1.0000000000000001e-05;
+    static const double ATOL = 1e-8;
 
-  double df = fabs(a - b);
-  double ref = ATOL + RTOL * fabs(b);
-  if (df > ref) {
-    printf("a=%.7f b=%.7f, df= %.3e, ref=%.3e\n", a, b, df, ref);
-  }
-  return df <= ref;
+    double df = ABS(a - b);
+    double ref = ATOL + RTOL * fabs(b);
+    if (df > ref) {
+        printf("a=%.7f b=%.7f, df= %.3e, ref=%.3e\n", a, b, df, ref);
+    }
+    return df <= ref;
 }
 
 // Compute relative error: ||computed - expected||/||expected||
 // Returns norm ||computed - exptected|| in dnorm. 
 // Note: contents of computed is destroyed.
-DTYPE rel_error(DTYPE *dnorm, armas_x_dense_t *computed,
-		  armas_x_dense_t *expected, int norm, int flags, armas_conf_t *conf)
+DTYPE rel_error(DTYPE * dnorm, armas_x_dense_t * computed,
+                armas_x_dense_t * expected, int norm, int flags,
+                armas_conf_t * conf)
 {
     double cnrm, enrm;
-    if (armas_x_is_vector(computed)) {
-      armas_x_axpy(computed, -1.0, expected, conf);
+    if (armas_x_isvector(computed)) {
+        armas_x_axpy(computed, -1.0, expected, conf);
     } else {
-      // computed = computed - expected
-      armas_x_scale_plus(1.0, computed, -1.0, expected, flags, conf);
+        // computed = computed - expected
+        armas_x_mplus(1.0, computed, -1.0, expected, flags, conf);
     }
     // ||computed - expected||
     cnrm = armas_x_mnorm(computed, norm, conf);
     if (dnorm)
-      *dnorm = cnrm;
+        *dnorm = cnrm;
     // ||expected||
     enrm = armas_x_mnorm(expected, norm, conf);
-    return cnrm/enrm;
+    return cnrm / enrm;
 }
 
-armas_x_dense_t *col_as_row(armas_x_dense_t *row, armas_x_dense_t *col)
+armas_x_dense_t *col_as_row(armas_x_dense_t * row, armas_x_dense_t * col)
 {
-  armas_x_make(row, 1, armas_x_size(col), 1, armas_x_data(col));
-  return row;
+    armas_x_make(row, 1, armas_x_size(col), 1, armas_x_data(col));
+    return row;
 }
 
 #if !defined(FLOAT32)
 // search from top-left
-void find_from_bottom_right(armas_d_dense_t *a, armas_d_dense_t *b, int *row, int *col)
+void find_from_bottom_right(armas_d_dense_t * a, armas_d_dense_t * b, int *row,
+                            int *col)
 {
-  int k;
-  armas_d_dense_t aC, bC;
+    int k;
+    armas_d_dense_t aC, bC;
 
-  for (k = a->rows-1; k >= 0; k--) {
-    armas_d_row(&aC, a, k);
-    armas_d_row(&bC, b, k);
-    if (! armas_d_allclose(&aC, &bC)) {
-      *row = k;
-      break;
+    for (k = a->rows - 1; k >= 0; k--) {
+        armas_d_row(&aC, a, k);
+        armas_d_row(&bC, b, k);
+        if (!armas_d_allclose(&aC, &bC)) {
+            *row = k;
+            break;
+        }
     }
-  }
-  if (*row == -1)
-    return;
+    if (*row == -1)
+        return;
 
-  for (k = 0; k < a->rows; k++) {
-    double a = armas_d_get(&aC, 0, k);
-    double b = armas_d_get(&bC, 0, k);
-    if ( ! in_tolerance(a, b)) {
-      *col = k;
-      break;
+    for (k = 0; k < a->rows; k++) {
+        double a = armas_d_get(&aC, 0, k);
+        double b = armas_d_get(&bC, 0, k);
+        if (!in_tolerance(a, b)) {
+            *col = k;
+            break;
+        }
     }
-  }
 }
 
-void find_from_top_left(armas_d_dense_t *a, armas_d_dense_t *b, int *row, int *col)
+void find_from_top_left(armas_d_dense_t * a, armas_d_dense_t * b, int *row,
+                        int *col)
 {
-  int k;
-  armas_d_dense_t aC, bC;
+    int k;
+    armas_d_dense_t aC, bC;
 
-  for (k = 0; k < a->cols; k++) {
-    armas_d_column(&aC, a, k);
-    armas_d_column(&bC, b, k);
-    if (! armas_d_allclose(&aC, &bC)) {
-      *col = k;
-      break;
+    for (k = 0; k < a->cols; k++) {
+        armas_d_column(&aC, a, k);
+        armas_d_column(&bC, b, k);
+        if (!armas_d_allclose(&aC, &bC)) {
+            *col = k;
+            break;
+        }
     }
-  }
-  if (*col == -1)
-    return;
+    if (*col == -1)
+        return;
 
-  // search row on column
-  for (k = 0; k < a->rows; k++) {
-    double a = armas_d_get(&aC, k, 0);
-    double b = armas_d_get(&bC, k, 0);
-    if ( ! in_tolerance(a, b)) {
-      *row = k;
-      break;
+    // search row on column
+    for (k = 0; k < a->rows; k++) {
+        double a = armas_d_get(&aC, k, 0);
+        double b = armas_d_get(&bC, k, 0);
+        if (!in_tolerance(a, b)) {
+            *row = k;
+            break;
+        }
     }
-  }
 }
 
-int check(armas_d_dense_t *A, armas_d_dense_t *B, int chkdir, const char *msg)
+int check(armas_d_dense_t * A, armas_d_dense_t * B, int chkdir, const char *msg)
 {
-  int ok = armas_d_allclose(A, B);
-  printf("%6s: %s\n", ok ? "OK" : "FAILED", msg);
-  if (ok)
+    int ok = armas_d_allclose(A, B);
+    printf("%6s: %s\n", ok ? "OK" : "FAILED", msg);
+    if (ok)
+        return ok;
+
+    armas_d_dense_t S;
+    int row = -1, col = -1;
+    if (chkdir > 0) {
+        // from top-left to bottom-right
+        find_from_top_left(A, B, &row, &col);
+    } else {
+        // from bottom-right to top-left
+        find_from_bottom_right(A, B, &row, &col);
+    }
+    printf("1: first difference at [%d, %d]\n", row, col);
+    armas_d_submatrix(&S, A, row, col,
+                      min(9, A->rows - row), min(9, A->cols - col));
+    printf("A, [%d, %d]\n", row, col);
+    armas_d_printf(stdout, "%9.2e", &S);
+
     return ok;
-
-  armas_d_dense_t S;
-  int row = -1, col = -1;
-  if (chkdir > 0) {
-    // from top-left to bottom-right
-    find_from_top_left(A, B, &row, &col);
-  } else {
-    // from bottom-right to top-left
-    find_from_bottom_right(A, B, &row, &col);
-  }
-  printf("1: first difference at [%d, %d]\n", row, col);
-  armas_d_submatrix(&S, A, row, col,
-		    min(9, A->rows-row), min(9, A->cols-col));
-  printf("A, [%d, %d]\n", row, col); armas_d_printf(stdout, "%9.2e", &S);
-
-  return ok;
 }
 
-void armas_x_printf(FILE *out, const char *efmt, const armas_d_dense_t *m, int partial)
+#if 0
+void armas_x_printf(FILE * out, const char *efmt, const armas_d_dense_t * m,
+                    int partial)
 {
-  int i, j;
-  if (!m)
-    return;
-  if (!efmt)
-    efmt = "%8.1e";
+    int i, j;
+    if (!m)
+        return;
+    if (!efmt)
+        efmt = "%8.1e";
 
-  int rowpartial = partial && m->rows > 18 ;
-  int colpartial = partial && m->cols > 9;
-  for (i = 0; i < (int)m->rows; i++ ) {
-    printf("[");
-    for (j = 0; j < (int)m->cols; j++ ) {
-      if (j > 0) {
-	printf(", ");
-      }
-      printf(efmt, m->elems[j*m->step+i]);
-      if (colpartial && j == 3) {
-        j = m->cols - 5;
-        printf(", ...");
-      }
+    int rowpartial = partial && m->rows > 18;
+    int colpartial = partial && m->cols > 9;
+    for (i = 0; i < (int) m->rows; i++) {
+        printf("[");
+        for (j = 0; j < (int) m->cols; j++) {
+            if (j > 0) {
+                printf(", ");
+            }
+            printf(efmt, m->elems[j * m->step + i]);
+            if (colpartial && j == 3) {
+                j = m->cols - 5;
+                printf(", ...");
+            }
+        }
+        printf("]\n");
+        if (rowpartial && i == 8) {
+            printf(" ....\n");
+            i = m->rows - 10;
+        }
     }
-    printf("]\n");
-    if (rowpartial && i == 8) {
-      printf(" ....\n");
-      i = m->rows - 10;
-    }
-  }
 }
 #endif
 
-
-
-
+#endif
---
+++ tests/testing.h
@@ -0,0 +1,91 @@
+
+#ifndef TEST_TESTING_H
+#define TEST_TESTING_H 1
+
+#include <math.h>
+#include <float.h>
+// type dependent mapping
+#include "dtype.h"
+#include "dlpack.h"
+#include "matrix.h"
+
+extern DTYPE zero(int i, int j);
+extern DTYPE one(int i, int j);
+extern DTYPE zeromean(int i, int j);
+extern DTYPE unitrand(int i, int j);
+extern DTYPE stdrand(int i, int j);
+extern DTYPE rowno(int i, int j);
+extern DTYPE colno(int i, int j);
+
+extern DTYPE rel_error(DTYPE * dnorm, armas_x_dense_t * computed,
+                       armas_x_dense_t * expected, int norm, int flags,
+                       armas_conf_t * conf);
+
+extern int check(armas_x_dense_t * A, armas_x_dense_t * B, int chkdir,
+                 const char *msg);
+extern armas_x_dense_t *col_as_row(armas_x_dense_t * row,
+                                   armas_x_dense_t * col);
+extern void armas_x_printf_partial(FILE * out, const char *efmt,
+                                   const armas_x_dense_t * m, int partial);
+
+
+extern int isOK(DTYPE nrm, int N);
+extern int isFINE(DTYPE nrm, DTYPE tol);
+
+
+extern double time_msec();
+extern double gflops(double ms, int64_t count);
+extern double flush();
+
+#define PASS(exp) ((exp) ? "PASS" : "FAIL")
+
+static inline
+int ndigits(double err)
+{
+    return err != 0.0 ? (int) (ceil(log10(1.0 / err))) : -1;
+}
+
+static inline
+int imin(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+static inline
+int imax(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+// extended precision helpers.
+
+extern DTYPE ep_dot_n(armas_x_dense_t * X, armas_x_dense_t * Y, DTYPE start,
+                      int N, int prec);
+extern DTYPE ep_dot(armas_x_dense_t * X, armas_x_dense_t * Y, DTYPE start,
+                    int prec);
+extern DTYPE ep_nrm2(armas_x_dense_t * X, DTYPE start, int prec);
+extern DTYPE ep_sum(armas_x_dense_t * X, DTYPE start, int prec);
+extern DTYPE ep_asum(armas_x_dense_t * X, DTYPE start, int prec);
+extern void ep_axpy(armas_x_dense_t * X, armas_x_dense_t * Y, DTYPE alpha,
+                    DTYPE beta, int prec);
+extern void ep_gemv(armas_x_dense_t * Y, armas_x_dense_t * A,
+                    armas_x_dense_t * X, DTYPE alpha, DTYPE beta, int prec,
+                    int flags);
+extern void ep_gemm(armas_x_dense_t * C, armas_x_dense_t * A,
+                    armas_x_dense_t * B, DTYPE alpha, DTYPE beta, int prec,
+                    int flags);
+
+extern void ep_gendot(double *dot, double *tcond,
+                      armas_x_dense_t * X, armas_x_dense_t * Y, double cond);
+extern void ep_gensum(double *dot, double *tcond, armas_x_dense_t * X,
+                      double cond);
+extern void ep_genmat(double *dot, double *tcond, armas_x_dense_t * A,
+                      armas_x_dense_t * B, double cond);
+extern void ep_gentrm(double *dot, double *tcond, armas_x_dense_t * A,
+                      armas_x_dense_t * B, double cond, int flags);
+
+#ifndef ARMAS_NULL
+#define ARMAS_NULL (armas_x_dense_t *)0
+#endif
+
+#endif /* TEST_TESTING_H */
