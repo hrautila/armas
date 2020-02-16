2f42eeba7e4b14b9b5aad96c7f60ca679ad08b29
Harri Rautila <harri.rautila@gmail.com>
2020-02-16 02:04:27 +0200

    Rewrite symmetric matrix multiplication tests.

--- tests/blas/symm.c
+++ tests/blas/symm.c
@@ -6,105 +6,195 @@
 
 #include "testing.h"
 
+#define ANULL (armas_x_dense_t *)0
+
+/*
+ * C = [M, N], A = [M, M], B = [M, N]
+ *
+ * test1:  symm(A, B) - gemm(A, B) == 0
+ */
+int test_left(int M, int N, int K, int verbose, armas_conf_t *cf)
+{
+    armas_x_dense_t A, B, C, Bt, T;
+    int ok, fails = 0;
+    DTYPE n0, n1;
+
+    armas_x_init(&C, M, N);
+    armas_x_init(&T, M, N);
+    armas_x_set_values(&C, zero, 0);
+
+    // test 1: M != N != K
+    armas_x_init(&A, M, M);
+    armas_x_init(&B, M, N);
+    armas_x_init(&Bt, N, M);
+    armas_x_set_values(&A, unitrand, ARMAS_SYMM);
+    armas_x_set_values(&B, unitrand, 0);
+    armas_x_mcopy(&Bt, &B, ARMAS_TRANS, cf);
+
+    armas_x_mult_sym(0.0, &C, 1.0, &A, &B, ARMAS_LEFT|ARMAS_UPPER, cf);
+    armas_x_mult(0.0, &T, 1.0, &A, &B, 0, cf);
+    n0 = rel_error(&n1, &C, &T, ARMAS_NORM_ONE, ARMAS_NONE, cf);
+
+    ok = n0 == 0.0 || isOK(n0, N) ? 1 : 0;
+    printf("%6s: symm(upper(A), B)   == gemm(A, B))\n", PASS(ok));
+    if (verbose > 0) {
+        printf("   || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+    fails += 1 - ok;
+
+    armas_x_mult_sym(0.0, &C, 1.0, &A, &Bt, ARMAS_LEFT|ARMAS_UPPER|ARMAS_TRANSB, cf);
+    armas_x_mult(0.0, &T, 1.0, &A, &Bt, ARMAS_TRANSB, cf);
+    n0 = rel_error(&n1, &C, &T, ARMAS_NORM_ONE, ARMAS_NONE, cf);
+
+    ok = n0 == 0.0 || isOK(n0, N) ? 1 : 0;
+    printf("%6s: symm(upper(A), B^T) == gemm(A, B^T))\n", PASS(ok));
+    if (verbose > 0) {
+        printf("   || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+    fails += 1 - ok;
+
+    armas_x_mult_sym(0.0, &C, 1.0, &A, &B, ARMAS_LEFT|ARMAS_LOWER, cf);
+    armas_x_mult(0.0, &T, 1.0, &A, &B, 0, cf);
+    n0 = rel_error(&n1, &C, &T, ARMAS_NORM_ONE, ARMAS_NONE, cf);
+
+    ok = n0 == 0.0 || isOK(n0, N) ? 1 : 0;
+    printf("%6s: symm(lower(A), B)   == gemm(A, B))\n", PASS(ok));
+    if (verbose > 0) {
+        printf("   || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+    fails += 1 - ok;
+
+    armas_x_mult_sym(0.0, &C, 1.0, &A, &Bt, ARMAS_LEFT|ARMAS_LOWER|ARMAS_TRANSB, cf);
+    armas_x_mult(0.0, &T, 1.0, &A, &Bt, ARMAS_TRANSB, cf);
+    n0 = rel_error(&n1, &C, &T, ARMAS_NORM_ONE, ARMAS_NONE, cf);
+
+    ok = n0 == 0.0 || isOK(n0, N) ? 1 : 0;
+    printf("%6s: symm(lower(A), B^T) == gemm(A, B^T))\n", PASS(ok));
+    if (verbose > 0) {
+        printf("   || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+    fails += 1 - ok;
+
+    return fails;
+}
+
+int test_right(int M, int N, int K, int verbose, armas_conf_t *cf)
+{
+    armas_x_dense_t A, B, C, Bt, T;
+    int ok, fails = 0;
+    DTYPE n0, n1;
+
+    armas_x_init(&C, M, N);
+    armas_x_init(&T, M, N);
+    armas_x_set_values(&C, zero, 0);
+
+    // test 1: M != N != K
+    armas_x_init(&A, N, N);
+    armas_x_init(&B, M, N);
+    armas_x_init(&Bt, N, M);
+    armas_x_set_values(&A, unitrand, ARMAS_SYMM);
+    armas_x_set_values(&B, unitrand, 0);
+    armas_x_mcopy(&Bt, &B, ARMAS_TRANS, cf);
+
+    armas_x_mult_sym(0.0, &C, 1.0, &A, &B, ARMAS_RIGHT|ARMAS_UPPER, cf);
+    armas_x_mult(0.0, &T, 1.0, &B, &A, 0, cf);
+    n0 = rel_error(&n1, &C, &T, ARMAS_NORM_ONE, ARMAS_NONE, cf);
+
+    ok = n0 == 0.0 || isOK(n0, N) ? 1 : 0;
+    printf("%6s: symm(B, upper(A))   == gemm(B, A))\n", PASS(ok));
+    if (verbose > 0) {
+        printf("   || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+    fails += 1 - ok;
+
+    armas_x_mult_sym(0.0, &C, 1.0, &A, &Bt, ARMAS_RIGHT|ARMAS_UPPER|ARMAS_TRANSB, cf);
+    armas_x_mult(0.0, &T, 1.0, &Bt, &A, ARMAS_TRANSA, cf);
+    n0 = rel_error(&n1, &C, &T, ARMAS_NORM_ONE, ARMAS_NONE, cf);
+
+    ok = n0 == 0.0 || isOK(n0, N) ? 1 : 0;
+    printf("%6s: symm(B^T, upper(A)) == gemm(B^T, A))\n", PASS(ok));
+    fails += 1 - ok;
+
+    armas_x_mult_sym(0.0, &C, 1.0, &A, &B, ARMAS_RIGHT|ARMAS_LOWER, cf);
+    armas_x_mult(0.0, &T, 1.0, &B, &A, 0, cf);
+    if (verbose > 0) {
+        printf("   || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+    n0 = rel_error(&n1, &C, &T, ARMAS_NORM_ONE, ARMAS_NONE, cf);
+
+    ok = n0 == 0.0 || isOK(n0, N) ? 1 : 0;
+    printf("%6s: symm(B, lower(A))   == gemm(B, A))\n", PASS(ok));
+    if (verbose > 0) {
+        printf("   || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+    fails += 1 - ok;
+
+    armas_x_mult_sym(0.0, &C, 1.0, &A, &Bt, ARMAS_RIGHT|ARMAS_LOWER|ARMAS_TRANSB, cf);
+    armas_x_mult(0.0, &T, 1.0, &Bt, &A, ARMAS_TRANSA, cf);
+    n0 = rel_error(&n1, &C, &T, ARMAS_NORM_ONE, ARMAS_NONE, cf);
+
+    ok = n0 == 0.0 || isOK(n0, N) ? 1 : 0;
+    printf("%6s: symm(B^T, lower(A)) == gemm(B^T, A))\n", PASS(ok));
+    if (verbose > 0) {
+        printf("   || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+    fails += 1 - ok;
+    return fails;
+}
+
+
 int main(int argc, char **argv)
 {
 
-    int ok, opt, verbose = 1, fails = 0;
-    int N = 633;
     armas_conf_t conf;
-    armas_x_dense_t C0, C1, A, As, B;
-    DTYPE n0, n1;
+
+    int opt;
+    int N = 387;
+    int M = 451;
+    int K = 337;
+    int verbose = 0;
+    int fails = 0;
+    conf = *armas_conf_default();
+    armas_env_t *env = armas_getenv();
+    env->fixed = 1;
+    env->mb = env->nb = env->kb = 4;
 
     while ((opt = getopt(argc, argv, "v")) != -1) {
         switch (opt) {
         case 'v':
-            verbose++;
+            verbose += 1;
             break;
         default:
-            fprintf(stderr, "usage: symm [-P nproc] [size]\n");
+            fprintf(stderr, "usage: test_symm [-v] [size]\n");
             exit(1);
         }
     }
 
-    if (optind < argc)
+    switch (argc-optind) {
+    case 3:
+        M = atoi(argv[optind]);
+        N = atoi(argv[optind+1]);
+        K = atoi(argv[optind+2]);
+        break;
+    case 2:
+        M = atoi(argv[optind]);
+        N = atoi(argv[optind+1]);
+        K = N;
+        break;
+    case 1:
         N = atoi(argv[optind]);
-
-    conf = *armas_conf_default();
-
-    armas_x_init(&C0, N, N);
-    armas_x_init(&C1, N, N);
-    armas_x_init(&A, N, N);
-    armas_x_init(&As, N, N);
-    armas_x_init(&B, N, N);
-
-    armas_x_set_values(&C0, zero, ARMAS_NULL);
-    armas_x_set_values(&C1, zero, ARMAS_NULL);
-    armas_x_set_values(&A, unitrand, ARMAS_SYMM);
-    armas_x_set_values(&B, unitrand, ARMAS_NULL);
-
-#if 0
-    if (verbose > 2 && N < 10) {
-        printf("A\n");
-        armas_x_printf(stdout, "%4.2f", &A);
-        printf("B\n");
-        armas_x_printf(stdout, "%4.2f", &B);
+        K = N;
+        M = N;
+        break;
+    default:
+        break;
     }
-#endif
-    // C0 = symm(upper(A)*B);  C1 = A*B
-    armas_x_mult_sym(0.0, &C0, 1.0, &A, &B, ARMAS_LEFT | ARMAS_UPPER, &conf);
-    armas_x_mult(0.0, &C1, 1.0, &A, &B, 0, &conf);
-
-    n0 = rel_error(&n1, &C0, &C1, ARMAS_NORM_ONE, ARMAS_NONE, &conf);
-    ok = (n0 == 0.0 || isOK(n0, N)) ? 1 : 0;
-
-    printf("%6s: symm(upper(A), B) == gemm(A, B)\n", PASS(ok));
-    if (verbose > 0) {
-        printf("   || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    if (optind < argc) {
     }
-    fails += 1 - ok;
 
-    // C = B*upper(A)
-    armas_x_mult_sym(0.0, &C0, 1.0, &A, &B, ARMAS_RIGHT | ARMAS_UPPER, &conf);
-    armas_x_mult(0.0, &C1, 1.0, &B, &A, 0, &conf);
 
-    n0 = rel_error(&n1, &C0, &C1, ARMAS_NORM_ONE, ARMAS_NONE, &conf);
-    ok = (n0 == 0.0 || isOK(n0, N)) ? 1 : 0;
-
-    printf("%6s: symm(B, upper(A)) == gemm(B, A)\n", PASS(ok));
-    if (verbose > 0) {
-        printf("   || rel error || : %e, [%d]\n", n0, ndigits(n0));
-    }
-    fails += 1 - ok;
-
-    // LOWER
-    armas_x_mult_sym(0.0, &C0, 1.0, &A, &B, ARMAS_LEFT | ARMAS_LOWER, &conf);
-    armas_x_mult(0.0, &C1, 1.0, &A, &B, 0, &conf);
-    n0 = rel_error(&n1, &C0, &C1, ARMAS_NORM_ONE, ARMAS_NONE, &conf);
-    ok = (n0 == 0.0 || isOK(n0, N)) ? 1 : 0;
-
-    printf("%6s: symm(lower(A), B) == gemm(A, B)\n", PASS(ok));
-    if (verbose > 0) {
-        printf("   || rel error || : %e, [%d]\n", n0, ndigits(n0));
-    }
-    fails += 1 - ok;
-    if (!ok)
-        exit(1 - ok);
-
-    // C = B*upper(A)
-    armas_x_mult_sym(0.0, &C0, 1.0, &A, &B, ARMAS_RIGHT | ARMAS_LOWER, &conf);
-    armas_x_mult(0.0, &C1, 1.0, &B, &A, 0, &conf);
-    n0 = rel_error(&n1, &C0, &C1, ARMAS_NORM_ONE, ARMAS_NONE, &conf);
-    ok = (n0 == 0.0 || isOK(n0, N)) ? 1 : 0;
-
-    printf("%6s: symm(B, lower(A)) == gemm(B, A)\n", PASS(ok));
-    if (verbose > 0) {
-        printf("   || rel error || : %e, [%d]\n", n0, ndigits(n0));
-    }
-    fails += 1 - ok;
+    fails += test_left(M, N, K, verbose, &conf);
+    fails += test_right(M, N, K, verbose, &conf);
 
     exit(fails);
 }
-
-// Local Variables:
-// indent-tabs-mode: nil
-// End:
