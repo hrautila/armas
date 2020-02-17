4d0685219d06133525ee57076b80f9ae4450992a
Harri Rautila <harri.rautila@gmail.com>
2020-02-18 00:13:19 +0200

    Add helper macro.

--- tests/testing.h
+++ tests/testing.h
@@ -84,8 +84,12 @@ extern void ep_genmat(double *dot, double *tcond, armas_x_dense_t * A,
 extern void ep_gentrm(double *dot, double *tcond, armas_x_dense_t * A,
                       armas_x_dense_t * B, double cond, int flags);
 
-#ifndef ARMAS_NULL
-#define ARMAS_NULL (armas_x_dense_t *)0
-#endif
+#define MAT_PRINT(s, A)                         \
+do {                                            \
+    if ((A)->cols < 10) {                       \
+        printf("%s\n", s);                      \
+        armas_x_printf(stdout, "%9.2e", (A));   \
+    }                                           \
+} while (0)
 
 #endif /* TEST_TESTING_H */
