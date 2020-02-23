c8556134532d09fab877f596be1b1fb489230e84
Harri Rautila <harri.rautila@gmail.com>
2020-02-23 21:04:18 +0200

    Mote to tests/lapack and reformat.

--- tests/lapack/housem.c
+++ tests/lapack/housem.c
@@ -30,25 +30,27 @@ int test_left(int flags, int verbose, int m, int n, int p)
     armas_d_init(&P, m, n);
     armas_d_set_values(&P, unitrand, 0);
     armas_d_set_values(&y, unitrand, 0);
-    armas_d_mcopy(&z, &y);
-    
+    armas_d_mcopy(&z, &y, 0, &cf);
+
     // generate Householder reflectors
     for (int k = 0; k < n; k++) {
         armas_x_submatrix(&x0, &P, k, k, 1, 1);
-        armas_x_submatrix(&x1, &P, k+1, k, m-k-1, 1);
+        armas_x_submatrix(&x1, &P, k + 1, k, m - k - 1, 1);
         //armas_x_set(&x0, 0, 0, -armas_x_get(&x0, 0, 0));
         armas_x_submatrix(&t, &tau, k, 0, 1, 1);
-        armas_x_house(&x0, &x1, &t,  flags, &cf);
+        armas_x_house(&x0, &x1, &t, flags, &cf);
     }
 
     // Q^T*(Q*y) == y
-    if (armas_x_housemult(&y, &tau, &P, flags|ARMAS_LEFT|ARMAS_TRANS, &cf) < 0)
+    int err = armas_x_housemult(&y, &tau, &P, flags | ARMAS_LEFT | ARMAS_TRANS, &cf);
+    if (err < 0)
         printf("left.1: error %d\n", cf.error);
-    if (armas_x_housemult(&y, &tau, &P, flags|ARMAS_LEFT, &cf) < 0)
+    err = armas_x_housemult(&y, &tau, &P, flags | ARMAS_LEFT, &cf);
+    if (err < 0)
         printf("left.2: error %d\n", cf.error);
 
     relerr = rel_error(&nrm, &z, &y, ARMAS_NORM_ONE, 0, &cf);
-    int ok = isFINE(relerr, m*__ERROR);
+    int ok = isFINE(relerr, m * __ERROR);
     char *s = (flags & ARMAS_UNIT) != 0 ? "unit scaled" : "   unscaled";
 
     printf("%s: (%s) y - Q.T*(Qy) == y\n", PASS(ok), s);
@@ -81,25 +83,27 @@ int test_right(int flags, int verbose, int m, int n, int p)
     armas_d_init(&P, m, n);
     armas_d_set_values(&P, unitrand, 0);
     armas_d_set_values(&y, unitrand, 0);
-    armas_d_mcopy(&z, &y);
-    
+    armas_d_mcopy(&z, &y, 0, &cf);
+
     // generate householder reflectors
     for (int k = 0; k < n; k++) {
         armas_x_submatrix(&x0, &P, k, k, 1, 1);
-        armas_x_submatrix(&x1, &P, k+1, k, m-k-1, 1);
+        armas_x_submatrix(&x1, &P, k + 1, k, m - k - 1, 1);
         //armas_x_set(&x0, 0, 0, -armas_x_get(&x0, 0, 0));
         armas_x_submatrix(&t, &tau, k, 0, 1, 1);
-        armas_x_house(&x0, &x1, &t,  flags, &cf);
+        armas_x_house(&x0, &x1, &t, flags, &cf);
     }
 
     // (y*Q^T)*Q == y
-    if (armas_x_housemult(&y, &tau, &P, flags|ARMAS_RIGHT|ARMAS_TRANS, &cf) < 0)
+    int err = armas_x_housemult(&y, &tau, &P, flags | ARMAS_RIGHT | ARMAS_TRANS, &cf);
+    if (err < 0)
         printf("right.1: error %d\n", cf.error);
-    if (armas_x_housemult(&y, &tau, &P, flags|ARMAS_RIGHT, &cf) < 0)
+    err = armas_x_housemult(&y, &tau, &P, flags | ARMAS_RIGHT, &cf);
+    if  (err < 0)
         printf("right.2: error %d\n", cf.error);
 
     relerr = rel_error(&nrm, &z, &y, ARMAS_NORM_INF, 0, &cf);
-    int ok = isFINE(relerr, m*__ERROR);
+    int ok = isFINE(relerr, m * __ERROR);
     char *s = (flags & ARMAS_UNIT) != 0 ? "unit scaled" : "   unscaled";
 
     printf("%s: (%s) y - (yQ.T)*Q == y\n", PASS(ok), s);
@@ -143,15 +147,10 @@ int main(int argc, char **argv)
             exit(1);
         }
     }
-    
+
     test_right(0, verbose, m, n, p);
     test_right(ARMAS_UNIT, verbose, m, n, p);
     test_left(0, verbose, m, n, p);
     test_left(ARMAS_UNIT, verbose, m, n, p);
     return 0;
 }
-
-// Local Variables:
-// c-basic-offset: 4
-// indent-tabs-mode: nil
-// End:
