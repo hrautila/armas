89c31922bf70d096a3e83bb653040fb887757425
Harri Rautila <harri.rautila@gmail.com>
2020-03-02 00:24:25 +0200

    Rewrite recursive version.

--- src/blas/trmm_rec.c
+++ src/blas/trmm_rec.c
@@ -28,6 +28,7 @@
 
 #include "matrix.h"
 #include "internal.h"
+#include "partition.h"
 
 // Recursive versions of TRMM
 
@@ -45,43 +46,33 @@
 static
 void mult_left_forward(
     armas_x_dense_t *B,
-    const armas_x_dense_t *A,
     DTYPE alpha,
+    const armas_x_dense_t *A,
     int flags,
     int min_mblock_size,
     cache_t *cache)
 {
-    armas_x_dense_t b0, b1, a0, a1;
-    int N = A->cols;
+    armas_x_dense_t ATL, ATR, ABL, ABR, BT, BB;
 
-    if (N < min_mblock_size) {
+    if (A->cols < min_mblock_size) {
         armas_x_trmm_unb(B, alpha, A, flags);
         return;
     }
 
-    armas_x_submatrix_unsafe(&b0, B, 0, 0, N/2, B->cols);
-    armas_x_submatrix_unsafe(&a0, A, 0, 0, N/2, N/2);
-    if (N/2 < min_mblock_size) {
-        armas_x_trmm_unb(&b0, alpha, &a0, flags);
-    } else {
-        mult_left_forward(&b0, &a0, alpha, flags, min_mblock_size, cache);
-    }
+    mat_partition_2x2(
+        &ATL, &ATR,
+        &ABL, &ABR, /**/ A, A->rows/2, A->cols/2, ARMAS_PTOPLEFT);
+    mat_partition_2x1(
+        &BT,
+        &BB, /**/ B, A->cols/2, ARMAS_PTOP);
 
-    // update B0 with A01/A10*B1
-    armas_x_submatrix_unsafe(&b1, B, N/2, 0, N-N/2, B->cols);
+    mult_left_forward(&BT, alpha, &ATL, flags, min_mblock_size, cache);
     if (flags & ARMAS_UPPER) {
-        armas_x_submatrix_unsafe(&a1, A, 0, N/2, N/2, N-N/2);
+        armas_x_mult_kernel_nc(&BT, alpha, &ATR, &BB, 0, cache);
     } else {
-        armas_x_submatrix_unsafe(&a1, A, N/2, 0, N-N/2, N/2);
+        armas_x_mult_kernel_nc(&BT, alpha, &ABL, &BB, ARMAS_TRANSA, cache);
     }
-    armas_x_mult_kernel_nc(&b0, alpha, &a1, &b1, flags, cache);
-
-    armas_x_submatrix_unsafe(&a1, A, N/2, N/2, N-N/2, N-N/2);
-    if (N/2 < min_mblock_size) {
-        armas_x_trmm_unb(&b1, alpha, &a1, flags);
-    } else {
-        mult_left_forward(&b1, &a1, alpha, flags, min_mblock_size, cache);
-    }
+    mult_left_forward(&BB, alpha, &ABR, flags, min_mblock_size, cache);
 }
 
 /*
@@ -98,42 +89,33 @@ void mult_left_forward(
 static
 void mult_left_backward(
     armas_x_dense_t *B,
-    const armas_x_dense_t *A,
     DTYPE alpha,
+    const armas_x_dense_t *A,
     int flags,
     int min_mblock_size,
     cache_t *cache)
 {
-    armas_x_dense_t b0, b1, a0, a1;
-    int N = A->cols;
-    if (N < min_mblock_size) {
+    armas_x_dense_t ATL, ATR, ABL, ABR, BT, BB;
+
+    if (A->cols < min_mblock_size) {
         armas_x_trmm_unb(B, alpha, A, flags);
         return;
     }
 
-    armas_x_submatrix_unsafe(&b1, B, N/2, 0, N-N/2, B->cols);
-    armas_x_submatrix_unsafe(&a1, A, N/2, N/2, N-N/2, N-N/2);
-    if (N/2 < min_mblock_size) {
-        armas_x_trmm_unb(&b1, alpha, &a1, flags);
-    } else {
-        mult_left_backward(&b1, &a1, alpha, flags, min_mblock_size, cache);
-    }
+    mat_partition_2x2(
+        &ATL, &ATR,
+        &ABL, &ABR, /**/ A, A->rows/2, A->cols/2, ARMAS_PBOTTOMRIGHT);
+    mat_partition_2x1(
+        &BT,
+        &BB, /**/ B, A->cols/2, ARMAS_PBOTTOM);
 
-    // update b1, with A10*B0/A01*b0
-    armas_x_submatrix_unsafe(&b0, B, 0, 0, N/2, B->cols);
+    mult_left_backward(&BB, alpha, &ABR, flags, min_mblock_size, cache);
     if (flags & ARMAS_UPPER) {
-        armas_x_submatrix_unsafe(&a0, A, 0, N/2, N/2, N-N/2);
+        armas_x_mult_kernel_nc(&BB, alpha, &ATR, &BT, ARMAS_TRANSA, cache);
     } else {
-        armas_x_submatrix_unsafe(&a0, A, N/2, 0, N-N/2, N/2);
+        armas_x_mult_kernel_nc(&BB, alpha, &ABL, &BT, 0, cache);
     }
-    armas_x_mult_kernel_nc(&b1, alpha, &a0, &b0, flags, cache);
-
-    armas_x_submatrix_unsafe(&a0, A, 0, 0, N/2, N/2);
-    if (N/2 < min_mblock_size) {
-        armas_x_trmm_unb(&b0, alpha, &a0, flags);
-    } else {
-        mult_left_backward(&b0, &a0, alpha, flags, min_mblock_size, cache);
-    }
+    mult_left_backward(&BT, alpha, &ATL, flags, min_mblock_size, cache);
 }
 
 /*
@@ -149,46 +131,35 @@ void mult_left_backward(
 static
 void mult_right_forward(
     armas_x_dense_t *B,
-    const armas_x_dense_t *A,
     DTYPE alpha,
+    const armas_x_dense_t *A,
     int flags,
     int min_mblock_size,
     cache_t *cache)
 {
-    armas_x_dense_t b0, b1, a0, a1;
-    int ops;
-    int N = A->cols;
+    armas_x_dense_t ATL, ATR, ABL, ABR, BL, BR;
+    //armas_x_dense_t b0, b1, a0, a1;
+    //int ops;
+    //int N = A->cols;
 
-    if (N < min_mblock_size) {
+    if (A->cols < min_mblock_size) {
         armas_x_trmm_unb(B, alpha, A, flags);
         return;
     }
 
-    armas_x_submatrix_unsafe(&a0, A, 0, 0, N/2, N/2);
-    armas_x_submatrix_unsafe(&b0, B, 0, 0, B->rows, N/2);
-    if (N/2 < min_mblock_size) {
-        armas_x_trmm_unb(&b0, alpha, &a0, flags);
-    } else {
-        mult_right_forward(&b0, &a0, alpha, flags, min_mblock_size, cache);
-    }
+    mat_partition_2x2(
+        &ATL, &ATR,
+        &ABL, &ABR, /**/ A, A->rows/2, A->cols/2, ARMAS_PTOPLEFT);
+    mat_partition_1x2(
+        &BL,  &BR,  /**/ B, A->cols/2, ARMAS_PLEFT);
 
+    mult_right_forward(&BL, alpha, &ATL, flags, min_mblock_size, cache);
     if (flags & ARMAS_UPPER) {
-        armas_x_submatrix_unsafe(&a1, A, 0, N/2, N/2, N-N/2);
+        armas_x_mult_kernel_nc(&BL, alpha, &BR, &ATR, ARMAS_TRANSB, cache);
     } else {
-        armas_x_submatrix_unsafe(&a1, A, N/2, 0, N-N/2, N/2);
+        armas_x_mult_kernel_nc(&BL, alpha, &BR, &ABL, 0, cache);
     }
-    armas_x_submatrix_unsafe(&b1, B, 0, N/2, B->rows, N-N/2);
-
-    ops = flags & ARMAS_TRANSA ? ARMAS_TRANSB : ARMAS_NULL;
-
-    armas_x_mult_kernel_nc(&b0, alpha, &b1, &a1, ops, cache);
-
-    armas_x_submatrix(&a1, A, N/2, N/2, N-N/2, N-N/2);
-    if (N/2 < min_mblock_size) {
-        armas_x_trmm_unb(&b1, alpha, &a1, flags);
-    } else {
-        mult_right_forward(&b1, &a1, alpha, flags, min_mblock_size, cache);
-    }
+    mult_right_forward(&BR, alpha, &ABR, flags, min_mblock_size, cache);
 }
 
 
@@ -205,43 +176,34 @@ void mult_right_forward(
 static
 void mult_right_backward(
     armas_x_dense_t *B,
-    const armas_x_dense_t *A,
     DTYPE alpha,
+    const armas_x_dense_t *A,
     int flags,
     int min_mblock_size,
     cache_t *cache)
 {
-  armas_x_dense_t b0, b1, a0, a1;
-  int N = A->cols;
+    armas_x_dense_t ATL, ATR, ABL, ABR, BL, BR;
+    //armas_x_dense_t b0, b1, a0, a1;
+    //int N = A->cols;
 
-  if (N < min_mblock_size) {
-    armas_x_trmm_unb(B, alpha, A, flags);
-    return;
-  }
+    if (A->cols < min_mblock_size) {
+        armas_x_trmm_unb(B, alpha, A, flags);
+        return;
+    }
 
-  armas_x_submatrix_unsafe(&b1, B, 0, N/2, B->rows, N-N/2);
-  armas_x_submatrix_unsafe(&a1, A, N/2, N/2, N-N/2, N-N/2);
-  if (N/2 < min_mblock_size) {
-    armas_x_trmm_unb(&b1, alpha, &a1, flags);
-  } else {
-    mult_right_backward(&b1, &a1, alpha, flags, min_mblock_size, cache);
-  }
+    mat_partition_2x2(
+        &ATL, &ATR,
+        &ABL, &ABR, /**/ A, A->rows/2, A->cols/2, ARMAS_PBOTTOMRIGHT);
+    mat_partition_1x2(
+        &BL,  &BR,  /**/ B, A->cols/2, ARMAS_PRIGHT);
 
-  if (flags & ARMAS_UPPER) {
-    armas_x_submatrix_unsafe(&a0, A, 0, N/2, N/2, N-N/2);
-  } else {
-    armas_x_submatrix_unsafe(&a0, A, N/2, 0, N-N/2, N/2);
-  }
-  armas_x_submatrix_unsafe(&b0, B, 0, 0, B->rows, N/2);
-  int flgs = flags & ARMAS_TRANSA ? ARMAS_TRANSB : 0;
-  armas_x_mult_kernel_nc(&b1, alpha, &b0, &a0, flgs, cache);
-
-  armas_x_submatrix_unsafe(&a0, A, 0, 0, N/2, N/2);
-  if (N/2 < min_mblock_size) {
-    armas_x_trmm_unb(&b0, alpha, &a0, flags);
-  } else {
-    mult_right_backward(&b0, &a0, alpha, flags, min_mblock_size, cache);
-  }
+    mult_right_backward(&BR, alpha, &ABR, flags, min_mblock_size, cache);
+    if (flags & ARMAS_UPPER) {
+        armas_x_mult_kernel_nc(&BR, alpha, &BL, &ATR, 0, cache);
+    } else {
+        armas_x_mult_kernel_nc(&BR, alpha, &BL, &ABL, ARMAS_TRANSB, cache);
+    }
+    mult_right_backward(&BL, alpha, &ATL, flags, min_mblock_size, cache);
 }
 
 void armas_x_trmm_recursive(
@@ -256,27 +218,26 @@ void armas_x_trmm_recursive(
     switch (flags&(ARMAS_UPPER|ARMAS_LOWER|ARMAS_RIGHT|ARMAS_TRANSA)) {
     case ARMAS_RIGHT|ARMAS_UPPER:
     case ARMAS_RIGHT|ARMAS_LOWER|ARMAS_TRANSA:
-        mult_right_backward(B, A, alpha, flags, env->blas2min, mcache);
+        mult_right_backward(B, alpha, A, flags, env->blas2min, mcache);
         break;
 
     case ARMAS_RIGHT|ARMAS_LOWER:
     case ARMAS_RIGHT|ARMAS_UPPER|ARMAS_TRANSA:
-        mult_right_forward(B, A, alpha, flags, env->blas2min, mcache);
+        mult_right_forward(B, alpha, A, flags, env->blas2min, mcache);
         break;
 
     case ARMAS_UPPER:
     case ARMAS_LOWER|ARMAS_TRANSA:
-        mult_left_forward(B, A, alpha, flags, env->blas2min, mcache);
+        mult_left_forward(B, alpha, A, flags, env->blas2min, mcache);
         break;
 
     case ARMAS_LOWER:
     case ARMAS_UPPER|ARMAS_TRANSA:
     default:
-        mult_left_backward(B, A, alpha, flags, env->blas2min, mcache);
+        mult_left_backward(B, alpha, A, flags, env->blas2min, mcache);
         break;
     }
 }
-
 #else
 #warning "Missing defines; no code!"
 #endif /* ARMAS_PROVIDES && ARMAS_REQUIRES */
