922d9c18e0dc2908ed2df8350327fee56c7af577
Harri Rautila <harri.rautila@gmail.com>
2020-03-11 00:02:01 +0200

    Move to tests/lapack and reformat.

--- tests/lapack/evd.c
+++ tests/lapack/evd.c
@@ -24,7 +24,7 @@ int test_1(int N, int flags, int verbose)
     int err, ok;
     char *uplo = flags & ARMAS_LOWER ? "L" : "U";
     armas_wbuf_t wb = ARMAS_WBNULL;
-    
+
     armas_x_init(&A, N, N);
     armas_x_init(&A0, N, N);
     armas_x_init(&V, N, N);
@@ -32,35 +32,38 @@ int test_1(int N, int flags, int verbose)
     armas_x_init(&D, N, 1);
 
     armas_x_set_values(&A, unitrand, ARMAS_SYMM);
-    armas_x_mcopy(&A0, &A);
+    armas_x_mcopy(&A0, &A, 0, &conf);
     nrm_A = armas_x_mnorm(&A0, ARMAS_NORM_ONE, &conf);
-    
-    if (armas_x_eigen_sym_w(&D, &A, flags|ARMAS_WANTV, &wb, &conf) < 0) {
+
+    if (armas_x_eigen_sym_w(&D, &A, flags | ARMAS_WANTV, &wb, &conf) < 0) {
         printf("eigen_sym: workspace calculation failure\n");
         return 0;
     }
     armas_walloc(&wb, wb.bytes);
-    
-    err = armas_x_eigen_sym(&D, &A, ARMAS_NULL, flags|ARMAS_WANTV, /*&wb,*/ &conf);
+
+    err = armas_x_eigen_sym(&D, &A, flags | ARMAS_WANTV, &conf);
     if (err) {
         printf("err = %d, %d\n", err, conf.error);
         return 0;
     }
-    
+
     armas_x_diag(&sD, &I0, 0);
-    armas_x_mult(0.0, &I0, 1.0, &A, &A, ARMAS_TRANSB, &conf);
-    armas_x_madd(&sD, -1.0, ARMAS_ANY);
+    armas_x_mult(ZERO, &I0, ONE, &A, &A, ARMAS_TRANSB, &conf);
+    armas_x_madd(&sD, -ONE, 0, &conf);
     n0 = armas_x_mnorm(&I0, ARMAS_NORM_ONE, &conf);
 
-    armas_x_mcopy(&V, &A);
-    armas_x_mult_diag(&V, 1.0, &D, ARMAS_RIGHT, &conf);
-    armas_x_mult(1.0, &A0, -1.0, &V, &A, ARMAS_TRANSB, &conf);
+    armas_x_mcopy(&V, &A, 0, &conf);
+    armas_x_mult_diag(&V, ONE, &D, ARMAS_RIGHT, &conf);
+    armas_x_mult(ONE, &A0, -ONE, &V, &A, ARMAS_TRANSB, &conf);
 
     if (N < 10 && verbose > 2) {
-        printf("D:\n"); armas_x_printf(stdout, "%6.3f", armas_x_col_as_row(&T, &D));
-        printf("I - V.T*V:\n"); armas_x_printf(stdout, "%6.3f", &I0);
-        printf("A - V*D*V.T:\n"); armas_x_printf(stdout, "%6.3f", &A0);
-    }        
+        printf("D:\n");
+        armas_x_printf(stdout, "%6.3f", armas_x_col_as_row(&T, &D));
+        printf("I - V.T*V:\n");
+        armas_x_printf(stdout, "%6.3f", &I0);
+        printf("A - V*D*V.T:\n");
+        armas_x_printf(stdout, "%6.3f", &A0);
+    }
     n1 = armas_x_mnorm(&A0, ARMAS_NORM_ONE, &conf);
     n1 /= nrm_A;
     ok = isOK(n1, N);
@@ -79,9 +82,6 @@ int test_1(int N, int flags, int verbose)
     return ok;
 }
 
-
-
-
 int main(int argc, char **argv)
 {
     int opt;
@@ -98,17 +98,17 @@ int main(int argc, char **argv)
             exit(1);
         }
     }
-    
+
     if (optind < argc) {
         N = atoi(argv[optind]);
     }
 
     int fails = 0;
 
-    if (! test_1(N, ARMAS_LOWER, verbose))
+    if (!test_1(N, ARMAS_LOWER, verbose))
         fails++;
 
-    if (! test_1(N, ARMAS_UPPER, verbose))
+    if (!test_1(N, ARMAS_UPPER, verbose))
         fails++;
 
     exit(fails);
