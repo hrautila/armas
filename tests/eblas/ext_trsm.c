69c38e7ac4884de88deacbbb1181d4b1ec187140
Harri Rautila <harri.rautila@gmail.com>
2020-03-24 00:07:49 +0200

    Add extended precision test for trsm.

--- tests/eblas/ext_trsm.c
+++ tests/eblas/ext_trsm.c
@@ -6,198 +6,187 @@
 #include <float.h>
 
 #include "testing.h"
-//#include <armas/dmatrix.h>
-//#include "helper.h"
 
-/*
- * Generate set matrices such that C = A * B ==> B = A.-1*C
- *
- *  UPPER:                       
- *   (b0 b0)  = (a00 a01) (x0)   
- *   (b1 b1)    ( 0   I ) (x1)   
- *
- *   b0 = a00*x0 + a01*x1 = a00*x0 + Dx
- *   b1 = I*x1 = x1
- *
- *   A is UNIT diagonal: 
- *     a00 = 1.0, x0 = Dx  ==> b0 = 2*Dx
- *   A is not UNIT diagonal:
- *     a00 = Dx,  x0 = 1.0 ==> b0 = 2*Dx
- *
- *  LOWER:                 
- *   (b0) = ( I   0 ) (x0)
- *   (b1)   (a10 a11) (x1)
- *
- *   b0 = x0
- *   b1 = a10*x0 + a11*x1 = Dx + a11*x1
- *
- *   A is UNIT diagonal: 
- *     a11 = 1.0, x1 = Dx  ==> b1 = 2*Dx
- *   A is not UNIT diagonal:
- *     a11 = Dx,  x1 = 1.0 ==> b1 = 2*Dx
- *
- */
-void ep_gentrsm(double *dot, double *tcond,
-                armas_x_dense_t *A, armas_x_dense_t *B, armas_x_dense_t *C,
-                double cond, int flags)
+int test_left_ext(int N, int K, int unit, int verbose, armas_conf_t *cf)
 {
-  armas_x_dense_t R0, C0, C1, D, Rx, Cx;
-  int k, tk;
-  int right = flags & ARMAS_RIGHT;
+    armas_d_dense_t Y, Y1, Y0, A, At, L, Lt, E, Et;
+    DTYPE n0, n1;
+    int ok, fails = 0;
 
-  armas_x_init(&R0, 0, 0);
-  armas_x_init(&C0, 0, 0);
-  // make A identity
-  armas_x_set_values(A, zero, 0);
-  armas_x_diag(&D, A, 0);
-  armas_x_set_values(&D, one, 0);
-  
-  switch (flags & (ARMAS_RIGHT|ARMAS_UPPER|ARMAS_LOWER|ARMAS_TRANS)) {
-  case ARMAS_LOWER|ARMAS_RIGHT:
-    armas_x_column(&R0, A, 0);
-    armas_x_subvector(&Rx, &R0, 1, armas_x_size(&R0)-1);
-    tk = 0;
-    break;
-  case ARMAS_LOWER|ARMAS_TRANS:
-    armas_x_column(&R0, A, 0);
-    armas_x_subvector(&Rx, &R0, 1, armas_x_size(&R0)-1);
-    tk = 0;
-    break;
-  case ARMAS_LOWER|ARMAS_TRANS|ARMAS_RIGHT:
-    armas_x_row(&R0, A, A->rows-1);
-    armas_x_subvector(&Rx, &R0, 0, armas_x_size(&R0)-1);
-    tk = B->cols - 1;
-    break;
-  case ARMAS_LOWER:
-    armas_x_row(&R0, A, A->rows-1);
-    armas_x_subvector(&Rx, &R0, 0, armas_x_size(&R0)-1);
-    tk = B->rows - 1;
-    break;
-  case ARMAS_UPPER|ARMAS_TRANS:
-    armas_x_column(&R0, A, A->cols-1);
-    armas_x_subvector(&Rx, &R0, 0, armas_x_size(&R0)-1);
-    tk = B->rows - 1;
-    break;
-  case ARMAS_UPPER|ARMAS_TRANS|ARMAS_RIGHT:
-    armas_x_row(&R0, A, 0);
-    armas_x_subvector(&Rx, &R0, 1, armas_x_size(&R0)-1);
-    tk = 0;
-    break;
-  case ARMAS_UPPER|ARMAS_RIGHT:
-    armas_x_column(&R0, A, A->cols-1);
-    armas_x_subvector(&Rx, &R0, 0, armas_x_size(&R0)-1);
-    tk = B->cols - 1;
-    break;
-  case ARMAS_UPPER:
-  default:
-    armas_x_row(&R0, A, 0);
-    armas_x_subvector(&Rx, &R0, 1, armas_x_size(&R0)-1);
-    tk = 0;
-    break;
-  }
+    armas_d_init(&Y, N, K);
+    armas_d_init(&Y0, N, K);
+    armas_d_init(&Y1, N, K);
+    armas_d_init(&E, N, K);
+    armas_d_init(&Et, N, K);
+    armas_d_init(&A, N, N);
+    armas_d_init(&At, N, N);
+    armas_d_init(&L, N, N);
+    armas_d_init(&Lt, N, N);
 
-  if (right) {
-    armas_x_row(&C0, B, 0);
-  } else {
-    armas_x_column(&C0, B, 0);
-  }
-  // generate dot product ...
-  armas_x_subvector(&Cx, &C0, tk == 0 ? 1 : 0, armas_x_size(&C0)-1);
-  ep_gendot(dot, tcond, &Rx, &Cx, cond);
+    armas_d_set_values(&Y, one, ARMAS_NULL);
+    armas_d_mcopy(&Y1, &Y, 0, cf);
+    make_ext_trsm_matrix(N, ARMAS_LEFT, &A, &At, &E, &Et, cf);
+    armas_d_mcopy(&Lt, &A, ARMAS_TRANS, cf);
+    armas_d_mcopy(&L, &At, ARMAS_TRANS, cf);
 
-  if (flags & ARMAS_UNIT) {
-    armas_x_set(A, tk, tk, 1.0);
-    armas_x_set_at(&C0, tk, *dot);
-  } else {
-    armas_x_set(A, tk, tk, *dot);
-    armas_x_set_at(&C0, tk, 1.0);
-  }
-
-  // make rest of rows/columns copies of first row/column.
-  for (k = 1; k < (right ? B->rows : B->cols); k++) {
-    if (right) {
-      armas_x_row(&C1, B, k);
-    } else {
-      armas_x_column(&C1, B, k);
+    if (verbose > 2 && N < 10) {
+        printf("U\n"); armas_d_printf(stdout, "%9.2e", &A);
+        printf("E\n"); armas_d_printf(stdout, "%9.2e", &E);
+        printf("Ut\n"); armas_d_printf(stdout, "%9.2e", &At);
+        printf("Et\n"); armas_d_printf(stdout, "%9.2e", &Et);
+        printf("L\n"); armas_d_printf(stdout, "%9.2e", &L);
+        printf("Lt\n"); armas_d_printf(stdout, "%9.2e", &Lt);
     }
-    armas_x_mcopy(&C1, &C0);
-  }
 
-  // create result matrix C; C = A*B where elements row/column at index tk
-  // have the value of 2*(dot product).
-  armas_x_mcopy(C, B);
-  if (right) {
-    armas_x_column(&C1, C, tk);
-  } else {
-    armas_x_row(&C1, C, tk);
-  }
-  //printf("..gentrsm tk=%d, dot=%13e, C1=[%ld]\n", tk, *dot, armas_x_size(&C1));
-  for (k = 0; k < armas_x_size(&C1); k++) {
-    armas_x_set_at(&C1, k, 2.0*(*dot));
-  }
+    // ---------------------------------------------------------
+#if 1
+    armas_d_mcopy(&Y0, &E, 0, cf);
+    armas_x_ext_solve_trm(&Y0, -1.0, &A, ARMAS_UPPER, cf);
+    armas_x_madd(&Y0, 1.0, 0, cf);
+
+    n0 = rel_error(&n1, &Y0, __nil, ARMAS_NORM_INF, 0, cf);
+    ok = n0 == 0.0 || isOK(n0, N);
+    fails += (1 - ok);
+    printf("%6s : expected == ext_trsm(X, LEFT|U)\n", PASS(ok));
+    if (verbose > 0) {
+        printf("    || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+#endif
+    // ---------------------------------------------------------
+#if 1
+    armas_d_mcopy(&Y0, &Et, 0, cf);
+    armas_x_ext_solve_trm(&Y0, -1.0, &At, ARMAS_UPPER|ARMAS_TRANS, cf);
+    armas_x_madd(&Y0, 1.0, 0, cf);
+    n0 = rel_error(&n1, &Y0, __nil, ARMAS_NORM_INF, 0, cf);
+
+    ok = n0 == 0.0 || isOK(n0, N);
+    fails += (1 - ok);
+    printf("%6s : expected == ext_trsm(X, LEFT|U|T)\n", PASS(ok));
+    if (verbose > 0) {
+        printf("    || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+#endif
+    // ---------------------------------------------------------
+    // ---------------------------------------------------------
+#if 1
+    armas_d_mcopy(&Y0, &Et, 0, cf);
+    armas_x_ext_solve_trm(&Y0, -1.0, &L, ARMAS_LOWER, cf);
+    armas_x_madd(&Y0, 1.0, 0, cf);
+    n0 = rel_error(&n1, &Y0, __nil, ARMAS_NORM_INF, 0, cf);
+
+    ok = n0 == 0.0 || isOK(n0, N);
+    fails += (1 - ok);
+    printf("%6s : expected == ext_trmm(X, LEFT|L)\n", PASS(ok));
+    if (verbose > 0) {
+        printf("    || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+#endif
+    // ---------------------------------------------------------
+#if 1
+    armas_d_mcopy(&Y0, &E, 0, cf);
+    armas_x_ext_solve_trm(&Y0, -1.0, &Lt, ARMAS_LOWER|ARMAS_TRANS, cf);
+    armas_x_madd(&Y0, 1.0, 0, cf);
+    n0 = rel_error(&n1, &Y0, __nil, ARMAS_NORM_INF, 0, cf);
+
+    ok = n0 == 0.0 || isOK(n0, N);
+    fails += (1 - ok);
+    printf("%6s : expected == ext_trmm(X, LEFT|L|T)\n", PASS(ok));
+    if (verbose > 0) {
+        printf("    || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+#endif
+    return fails;
 }
 
-// ne = norm1 of exact; re = ||exact-result||/||exact||
-void compute(double *ne, double *re,
-             armas_x_dense_t *B, armas_x_dense_t *A, armas_x_dense_t *C,
-             int flags, int prec, int verbose, armas_conf_t *conf)
+int test_right_ext(int N, int K, int unit, int verbose, armas_conf_t *cf)
 {
-  DTYPE e;
-  // B = A*B
-  armas_x_solve_trm(B, 1.0, A, flags, conf);
-  if (verbose > 1 && B->cols < 10) {
-    printf("A.-1*B:\n"); armas_x_printf(stdout, "%13e", B);
-  }
-  *re = rel_error(&e, B, C, ARMAS_NORM_INF, ARMAS_NONE, conf);
-  if (ne)
-    *ne = e;
-}
+    armas_d_dense_t Y0, A, At, L, Lt, E, Et;
+    DTYPE n0, n1;
+    int ok, fails = 0;
 
-int test(char *name, int N, int K, int flags, int verbose, int prec, double cwant, armas_conf_t *conf)
-{
-  armas_x_dense_t B0, A, B, Ce;
-  double dot, cond, m_c, m_one;
-  int ok;
+    armas_d_init(&Y0, K, N);
+    armas_d_init(&E, K, N);
+    armas_d_init(&Et, K, N);
+    armas_d_init(&A, N, N);
+    armas_d_init(&At, N, N);
+    armas_d_init(&L, N, N);
+    armas_d_init(&Lt, N, N);
 
+    armas_d_set_values(&Y0, one, ARMAS_NULL);
+    make_ext_trsm_matrix(N, ARMAS_RIGHT, &A, &At, &E, &Et, cf);
+    armas_d_mcopy(&Lt, &A, ARMAS_TRANS, cf);
+    armas_d_mcopy(&L, &At, ARMAS_TRANS, cf);
 
-  armas_x_init(&A, N, N);
-  if (flags & ARMAS_RIGHT) {
-    armas_x_init(&Ce, K, N);
-    armas_x_init(&B, K, N);
-    armas_x_init(&B0, K, N);
-  } else {
-    armas_x_init(&Ce, N, K);
-    armas_x_init(&B, N, K);
-    armas_x_init(&B0, N, K);
-  }
-  
-  ep_gentrsm(&dot, &cond, &A, &B0, &B, cwant, flags);
-
-  if (verbose > 1 && N < 10) {
-    printf("A:\n"); armas_x_printf(stdout, "%13e", &A);
-    if (K < 10) {
-      printf("B0:\n"); armas_x_printf(stdout, "%13e", &B0);
-      printf("B:\n"); armas_x_printf(stdout, "%13e", &B);
+    if (verbose > 2 && N < 10) {
+        printf("U\n"); armas_d_printf(stdout, "%9.2e", &A);
+        printf("E\n"); armas_d_printf(stdout, "%9.2e", &E);
+        printf("Ut\n"); armas_d_printf(stdout, "%9.2e", &At);
+        printf("Et\n"); armas_d_printf(stdout, "%9.2e", &Et);
+        printf("L\n"); armas_d_printf(stdout, "%9.2e", &L);
+        printf("Lt\n"); armas_d_printf(stdout, "%9.2e", &Lt);
     }
-  }
 
-  // extended precision computations
+    // ---------------------------------------------------------
+#if 1
+    armas_d_mcopy(&Y0, &E, 0, cf);
+    armas_x_ext_solve_trm(&Y0, -1.0, &A, ARMAS_RIGHT|ARMAS_UPPER, cf);
+    if (verbose > 2 && N < 10) {
+        printf("Y*A^-1:\n"); armas_d_printf(stdout, "%9.2e", &Y0);
+    }
+    armas_x_madd(&Y0, 1.0, 0, cf);
 
-  // 1. A*B
-  compute(&m_c, &m_one, &B, &A, &B0, flags, prec, verbose, conf);
+    n0 = rel_error(&n1, &Y0, __nil, ARMAS_NORM_INF, 0, cf);
+    ok = n0 == 0.0 || isOK(n0, N);
+    fails += (1 - ok);
+    printf("%6s : expected == ext_trsm(X, RIGHT|U)\n", PASS(ok));
+    if (verbose > 0) {
+        printf("    || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+#endif
+    // ---------------------------------------------------------
+#if 1
+    armas_d_mcopy(&Y0, &Et, 0, cf);
+    armas_x_ext_solve_trm(&Y0, -1.0, &At, ARMAS_RIGHT|ARMAS_UPPER|ARMAS_TRANS, cf);
+    armas_x_madd(&Y0, 1.0, 0, cf);
+    n0 = rel_error(&n1, &Y0, __nil, ARMAS_NORM_INF, 0, cf);
 
-  ok = m_one < N*_EPS;
-  printf("%-4s: %s rel.error %e [%e]\n",  PASS(ok), name, m_one, m_c);
-  if (!ok && K < 10) {
-    printf("B-Ce:\n"); armas_x_printf(stdout, "%13e", &B);
-  }
+    ok = n0 == 0.0 || isOK(n0, N);
+    fails += (1 - ok);
+    printf("%6s : expected == ext_trsm(X, RIGHT|U|T)\n", PASS(ok));
+    if (verbose > 0) {
+        printf("    || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+#endif
+    // ---------------------------------------------------------
+    // ---------------------------------------------------------
+#if 1
+    armas_d_mcopy(&Y0, &Et, 0, cf);
+    armas_x_ext_solve_trm(&Y0, -1.0, &L, ARMAS_RIGHT|ARMAS_LOWER, cf);
+    armas_x_madd(&Y0, 1.0, 0, cf);
+    n0 = rel_error(&n1, &Y0, __nil, ARMAS_NORM_INF, 0, cf);
 
-  armas_x_release(&Ce);
-  armas_x_release(&A);
-  armas_x_release(&B);
-  armas_x_release(&B0);
+    ok = n0 == 0.0 || isOK(n0, N);
+    fails += (1 - ok);
+    printf("%6s : expected == ext_trmm(X, RIGHT|L)\n", PASS(ok));
+    if (verbose > 0) {
+        printf("    || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+#endif
+    // ---------------------------------------------------------
+#if 1
+    armas_d_mcopy(&Y0, &E, 0, cf);
+    armas_x_ext_solve_trm(&Y0, -1.0, &Lt, ARMAS_RIGHT|ARMAS_LOWER|ARMAS_TRANS, cf);
+    armas_x_madd(&Y0, 1.0, 0, cf);
+    n0 = rel_error(&n1, &Y0, __nil, ARMAS_NORM_INF, 0, cf);
 
-  return ok;
+    ok = n0 == 0.0 || isOK(n0, N);
+    fails += (1 - ok);
+    printf("%6s : expected == ext_trmm(X, RIGHT|L|T)\n", PASS(ok));
+    if (verbose > 0) {
+        printf("    || rel error || : %e, [%d]\n", n0, ndigits(n0));
+    }
+#endif
+    return fails;
 }
 
 /*
@@ -206,107 +195,37 @@ int test(char *name, int N, int K, int flags, int verbose, int prec, double cwan
 int main(int argc, char **argv)
 {
 
-  armas_conf_t conf;
+    armas_conf_t cf;
 
-  int ok, opt;
-  int N = 33;
-  int K = 33;
-  int fails = 0;
-  int normal_prec = 0;
-  int prec = 200;
-  int verbose = 0;
-  int flags = ARMAS_UPPER;
-  int all = 1;
-  int unit = 0;
-  int naive = 0;
-  double cwant = 1.0/_EPS; // wanted condition number
+    int opt;
+    int N = 33;
+    int K = 33;
+    int fails = 0;
+    int verbose = 0;
 
-  while ((opt = getopt(argc, argv, "C:p:vnASsLRTU")) != -1) {
-    switch (opt) {
-    case 'C':
-      cwant = strtod(optarg, (char **)0);
-      break;
-    case 'p':
-      prec = atoi(optarg);
-      break;
-    case 's':
-      normal_prec = 1;
-      break;
-    case 'n':
-      naive = 1;
-      break;
-    case 'v':
-      verbose += 1;
-      break;
-    case 'S':
-      all = 0;
-      break;
-    case 'L':
-      flags &= ~ARMAS_UPPER;
-      flags |= ARMAS_LOWER;
-      break;
-    case 'R':
-      flags &= ~ARMAS_LEFT;
-      flags |= ARMAS_RIGHT;
-      break;
-    case 'T':
-      flags |= ARMAS_TRANS;
-      break;
-    case 'U':
-      flags |= ARMAS_UNIT;
-      unit = ARMAS_UNIT;
-      break;
-    default:
-      fprintf(stderr, "usage: ext_trsm [-p bits -C cond -v -SLRT] [size]\n");
-      exit(1);
+    while ((opt = getopt(argc, argv, "v")) != -1) {
+        switch (opt) {
+        case 'v':
+            verbose += 1;
+            break;
+        default:
+            fprintf(stderr,
+                    "usage: ext_trsm [-p bits -C cond -v -SLRT] [size]\n");
+            exit(1);
+        }
     }
-  }
-    
-  if (optind < argc-1) {
-    N = atoi(argv[optind]);
-    K = atoi(argv[optind+1]);
-  } else if (optind < argc) {
-    N = atoi(argv[optind]);
-    K = N;
-  }
-  
-  if (verbose > 1) {
-    printf("flags = 0x%X\n", flags);
-  }
-  conf = *armas_conf_default();
-  if (!normal_prec)
-    conf.optflags |= ARMAS_OEXTPREC;
-  if (naive)
-    conf.optflags |= ARMAS_ONAIVE;
 
-  if (!all) {
-    //printf("conf .mb, .nb, .kb: %d, %d, %d\n", conf.mb, conf.nb, conf.kb);
-    if (flags & ARMAS_UNIT)
-      printf("UNIT matrix A...\n");
-    ok = test("single", N, K, flags, verbose, prec, cwant, &conf);
-    fails += 1 - ok;
-  } else {
-    ok = test("u(A)*B  ", N, K, unit|ARMAS_UPPER, verbose, prec, cwant, &conf);
-    fails += 1 - ok;
-    ok = test("l(A)*B  ", N, K, unit|ARMAS_LOWER, verbose, prec, cwant, &conf);
-    fails += 1 - ok;
-    ok = test("u(A.T)*B", N, K, unit|ARMAS_UPPER|ARMAS_TRANS, verbose, prec, cwant, &conf);
-    fails += 1 - ok;
-    ok = test("l(A.T)*B", N, K, unit|ARMAS_LOWER|ARMAS_TRANS, verbose, prec, cwant, &conf);
-    fails += 1 - ok;
-    ok = test("B*u(A)  ", N, K, unit|ARMAS_RIGHT|ARMAS_UPPER, verbose, prec, cwant, &conf);
-    fails += 1 - ok;
-    ok = test("B*l(A)  ", N, K, unit|ARMAS_RIGHT|ARMAS_LOWER, verbose, prec, cwant, &conf);
-    fails += 1 - ok;
-    ok = test("B*u(A.T)", N, K, unit|ARMAS_RIGHT|ARMAS_UPPER|ARMAS_TRANS, verbose, prec, cwant, &conf);
-    fails += 1 - ok;
-    ok = test("B*l(A.T)", N, K,unit| ARMAS_RIGHT|ARMAS_LOWER|ARMAS_TRANS, verbose, prec, cwant, &conf);
-    fails += 1 - ok;
-  }
+    if (optind < argc - 1) {
+        N = atoi(argv[optind]);
+        K = atoi(argv[optind + 1]);
+    } else if (optind < argc) {
+        N = atoi(argv[optind]);
+        K = N;
+    }
 
-  exit(fails);
+    cf = *armas_conf_default();
+    fails += test_left_ext(N, K, 0, verbose, &cf);
+    fails += test_right_ext(N, K, 0, verbose, &cf);
+
+    exit(fails);
 }
-
-// Local Variables:
-// indent-tabs-mode: nil
-// End:
